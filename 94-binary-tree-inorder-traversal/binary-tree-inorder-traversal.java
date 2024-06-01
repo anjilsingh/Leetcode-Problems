d3c5fb5ad14ad220b64af01cf8ec4ae7a88b15d0
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-06-01 06:50:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (5.95%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -17,33 +17,70 @@ class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         
         List<Integer>in=new ArrayList<>();
-        inHelper(root,in);
+        Stack<TreeNode>st=new Stack<TreeNode>();
+        TreeNode node=root;
+        while(true){
+           if(node!=null){
+             st.push(node);
+             node=node.left;
+           }
+            else{
+               if(st.empty()){
+                 break ;
+               }
+          node=st.pop();
+                in.add(node.val);
+                node=node.right;
+            }
+        
+        
+        }
+        
         return in;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         inHelper(root,in);
+//         return in;
         
      
         
         
-//           List<Integer> result = new ArrayList<>();
-//         Stack<TreeNode> stack = new Stack<>();
+// //           List<Integer> result = new ArrayList<>();
+// //         Stack<TreeNode> stack = new Stack<>();
 
-//         while (root != null || !stack.isEmpty()) {
-//             while (root != null) {
-//                 stack.push(root);
-//                 root = root.left;
-//             }
-//             root = stack.pop();
-//             result.add(root.val);
-//             root = root.right;
-//         }
+// //         while (root != null || !stack.isEmpty()) {
+// //             while (root != null) {
+// //                 stack.push(root);
+// //                 root = root.left;
+// //             }
+// //             root = stack.pop();
+// //             result.add(root.val);
+// //             root = root.right;
+// //         }
 
-//         return result;
-    }
+// //         return result;
+//     }
     
-    private static void  inHelper(TreeNode root,List<Integer>list){
-        if(root==null){
- return;}
-       inHelper(root.left,list);
-    list.add(root.val);
-        inHelper(root.right,list);
+//     private static void  inHelper(TreeNode root,List<Integer>list){
+//         if(root==null){
+//  return;}
+//        inHelper(root.left,list);
+//     list.add(root.val);
+//         inHelper(root.right,list);
     }
 }
