a98993ee4fa4f55dadc3f0260e6281eb82e61fea
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-08-25 09:02:20 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (64.59%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,92 +15,11 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer>post=new ArrayList<>();
-        if(root==null){
-                 return post;
-        }
-        Stack<TreeNode>st1=new Stack<TreeNode>();
-        TreeNode cur=root;
-        while((cur!=null)||(!st1.empty())){
-
-           if(cur!=null){
-               st1.push(cur);
-               cur=cur.left;
-           }
-           else{
-             TreeNode temp=st1.peek().right;
-              if(temp==null){
-                  temp=st1.pop();
-                  post.add(temp.val);
-                  while(!st1.empty() && temp==st1.peek().right){
-                      temp=st1.pop();
-                      post.add(temp.val);
-                  }
-              
-              
-              }
-               else{
-                        cur=temp;
-               }
-           }
-        
-        }
-              
-              return post;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//         Stack<TreeNode>st2=new Stack<TreeNode>();
-//           if (root == null) {
-//             return post;
-//         }
-//         st1.push(root);
-//         while(!st1.empty()){
-//             TreeNode node=st1.pop();
-//             st2.add(node);
-//            if(node.left!=null) st1.add(node.left);
-//            if(node.right!=null) st1.add(node.right);
-            
-//         }
-//         while(!st2.empty()){
-//           TreeNode n=st2.pop();
-//             post.add(n.val);
-            
-//         }
-        
-//         return post;
-        
-//         postHelper(root,post);
-//         return post;
-        
-//     }
-//     private static void postHelper(TreeNode root,List<Integer>list){
-//    if(root==null){
-//      return ;
-//    }
-//         postHelper(root.left,list);
-//         postHelper(root.right,list);
-//         list.add(root.val);
+        if (root == null) return new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        result.addAll(postorderTraversal(root.left));
+        result.addAll(postorderTraversal(root.right));
+        result.add(root.val);
+        return result;
     }
 }
