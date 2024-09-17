5613d7d56a956418835216a9d3accdbb8cd7a382
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-09-17 21:36:25 +0530

    Time: 16 ms (38.77%) | Memory: 58.7 MB (40.97%) - LeetSync

--- 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.java
+++ 2661-smallest-missing-non-negative-integer-after-operations/smallest-missing-non-negative-integer-after-operations.java
@@ -1,22 +1,34 @@
+
 class Solution {
     public int findSmallestInteger(int[] nums, int value) {
-        
-        int vis[]=new int[value];
-        for(int it:nums){
-            if(it%value<0) vis[(it%value)+value]++;
-           else vis[Math.abs(it)%value]++; 
+        // Step 1: Change all negative numbers to the nearest positive equivalent
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                nums[i] += ((Math.abs(nums[i]) / value) * value);
+                if (nums[i] < 0) {
+                    nums[i] += value;
+                }
+            }
         }
-        
-        
-        int res=0;
-        
-        while(true){
-            
-            if(vis[res%value]==0) return res;
-            vis[res%value]--;
-            res++;
+
+        // Step 2: Create an array to store the frequency of remainders
+        int[] a = new int[100001]; // Array to store remainder frequencies, initialized to 0
+
+        // Step 3: Store frequencies of all remainders when divided by 'value'
+        for (int num : nums) {
+            a[num % value]++;
         }
-        
-        // return -1;
+
+        // Step 4: Find the smallest missing remainder based on frequencies
+        int n = nums.length / value + 1;  // Calculating an upper bound for iterations
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < value; i++) {
+                if (a[i] == j) {
+                    return value * j + i;  // Return the smallest missing integer
+                }
+            }
+        }
+
+        return 0;  // In case no value is found (which is unlikely given the logic)
     }
 }
