721f7556dda3dd8f4b0d3abeff73ebb2a5140752
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-09-18 08:54:28 +0530

    Time: 1 ms (27.32%) | Memory: 41.9 MB (5.08%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,27 @@
 class Solution {
+    public boolean compare(String s1,String s2){
+          for(int i=0;i<s1.length();i++){
+             if(s1.charAt(i)!=s2.charAt(i)){
+                  return false;
+             }
+          }
+        return true;
+    }
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+         if(haystack.length()<needle.length()){
+            return -1;
+         }
+        for(int i=0;i<haystack.length();i++){
+            char x=haystack.charAt(i);
+            if(x==needle.charAt(0) && i+needle.length()<=haystack.length()){
+             boolean res=compare(haystack.substring(i,i+needle.length()),needle);
+             if(res)
+              {
+                return i;
+                  
+              }
+            }
+        }
+        return -1;
     }
 }
