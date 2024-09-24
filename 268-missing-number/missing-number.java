b0249cc69a1d7914b4cbe55b005188418d1b0508
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-09-24 23:50:08 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (84.91%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,17 +1,28 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int ans=-1;  
-        Map<Integer,Integer>mp=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            mp.put(nums[i],i);
-        }
-        //now iterate through the numberrs from 1 to n and if it is not present in hashmap retturn that number
-        for(int i=0;i<=nums.length;i++){
+        //first method tc,sc-O(N) using hashmap
+//         int ans=-1;  
+//         Map<Integer,Integer>mp=new HashMap<>();
+//         for(int i=0;i<nums.length;i++){
+//             mp.put(nums[i],i);
+//         }
+//         //now iterate through the numberrs from 1 to n and if it is not present in hashmap retturn that number
+//         for(int i=0;i<=nums.length;i++){
             
-            if(!mp.containsKey(i)){
-                ans=i;
-            }
+//             if(!mp.containsKey(i)){
+//                 ans=i;
+//             }
+//         }
+//         return ans;
+        
+        //second method 
+        //in second mehtod we can take the sum of given string ans the sum of first n natural no and subtract those by this we can find the missing number 
+        int arrSum=0;
+        for(int i=0;i<nums.length;i++){
+            arrSum+=nums[i];
         }
-        return ans;
+        int orgSum=(nums.length)*(nums.length+1)/2;
+        
+        return orgSum-arrSum;
     }
 }
