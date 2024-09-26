e430c8a0e380d24e8f48f633ab9e9017aa8700e9
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-09-27 00:53:21 +0530

    Time: 396 ms (24.01%) | Memory: 51.8 MB (43.43%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,33 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        
-        // Sort the array to handle duplicates easily later
+        //create a list of list to store the results        
+        List<List<Integer>>ans=new ArrayList<>();
+        //sort the array to remove the potential duplication
         Arrays.sort(nums);
-        
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicates for the current number
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            
-            int target = -nums[i];
-            HashMap<Integer, Integer> map = new HashMap<>();
-            
-            for (int j = i + 1; j < nums.length; j++) {
-                int complement = target - nums[j];
-                
-                // Check if the complement exists in the map
-                if (map.containsKey(complement)) {
-                    result.add(Arrays.asList(nums[i], nums[j], complement));
-                    
-                    // Skip duplicates for the second number
-                    while (j + 1 < nums.length && nums[j] == nums[j + 1]) j++;
-                }
-                
-                // Add the current number to the map
-                map.put(nums[j], j);
+        //now iterate through one one element 
+       for(int i=0;i<nums.length-2;i++){
+             if(i>0 && nums[i]==nums[i-1]) continue;   //ignore duplicate values
+             int target=-nums[i];
+             HashMap<Integer,Integer>mp=new HashMap<>();
+        for(int j=i+1;j<nums.length;j++){
+             int com=target-nums[j];
+             if(mp.containsKey(com)){
+               ans.add(Arrays.asList(nums[i],nums[j],com));
+               while(j+1<nums.length && nums[j]==nums[j+1]) j++;
             }
-        }
+                mp.put(nums[j],j);
+             
+    }
+
         
-        return result;
+        
+             
+}
+        return ans;
+        
     }
 }
