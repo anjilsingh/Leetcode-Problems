2b9c8bff640e8afeaf96581780a7235f76dde77a
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-02 09:10:23 +0530

    Time: 1 ms (50.99%) | Memory: 57.3 MB (49.19%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,28 +1,22 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n=nums.length;
-        if(n==0){
-            return;
-        }
-        k=k%n;
-        if(k>n){
-          return;
-        }
-        //create an empty array to store last k values
-        int nums1[]=new int[k];
+         k=k%n;
+        
+        //storing the last k elements
+        int temp[]=new int[k];
         int j=0;
         for(int i=n-k;i<n;i++){
-           nums1[j]=nums[i];
-           j++;
-        }
-        //right shift starting n-k elemnts
-        for(int i=n-k-1;i>=0;i--){
-             nums[i+k]=nums[i];
-        }
-       //put stores values in front of original array
-        for(int i=0;i<k;i++){
-            nums[i]=nums1[i];
+             temp[j++]=nums[i];
         }
         
+        //Now right rotate starting n-k elements
+        for(int i=n-1;i>=k;i--){
+              nums[i]=nums[i-k];    
+        }
+        //now put last 3 ele at starting 
+        for(int i=0;i<k;i++){
+            nums[i]=temp[i];
+        }
     }
 }
