c8a1fdda8420273ed18794508058457ed4fdf235
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-03 16:05:38 +0530

    Time: 4 ms (47.86%) | Memory: 76.7 MB (75.24%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,20 +1,23 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        ArrayList<Integer>pos=new ArrayList<>();
-        ArrayList<Integer>neg=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>=0){
-                pos.add(nums[i]);
-            }
-            else{
-             neg.add(nums[i]);
+        int[] result = new int[nums.length]; // New array to hold the rearranged numbers
+        int pos = 0;  // Position for placing positive numbers (even indices)
+        int neg = 1;  // Position for placing negative numbers (odd indices)
+
+        // Loop through the original array
+        for (int i = 0; i < nums.length; i++) {
+            // Place positive numbers at even indices
+            if (nums[i] > 0 && pos < nums.length) {
+                result[pos] = nums[i];
+                pos += 2; // Move to the next even index
+            } 
+            // Place negative numbers at odd indices
+            else if (nums[i] < 0 && neg < nums.length) {
+                result[neg] = nums[i];
+                neg += 2; // Move to the next odd index
             }
         }
-      
-        for(int i=0;i<nums.length/2;i++){
-          nums[2*i]=pos.get(i);
-          nums[2*i+1]=neg.get(i);
-        }
-        return nums;
+
+        return result; // Return the rearranged array
     }
 }
