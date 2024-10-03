3146a72b58fbf0f0275e7e58d2392a59195f3420
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-03 22:47:25 +0530

    Time: 400 ms (23.87%) | Memory: 51.3 MB (77.00%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,27 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-      
-        //the idea behind this is , a+b+c=0 , so we get -a-b value and then search for c in the hashmap if it is present then we will add the result in the array List
-        
-        List<List<Integer>>ans=new ArrayList<>();
-        Arrays.sort(nums);
+      List<List<Integer>>ans=new ArrayList<>();
+      Arrays.sort(nums);
+      int n=nums.length;
+     for(int i=0;i<n-2;i++){
+        if(i>0 && nums[i]==nums[i-1]) continue;
+         HashMap<Integer,Integer>mp=new HashMap<>();
+         int x=-nums[i];
+         
+         for(int j=i+1;j<n;j++){
+             int tar=x-nums[j];
+             if(mp.containsKey(tar)){
+                ans.add(Arrays.asList(nums[i],nums[j],tar));
+                 while(j+1<n && nums[j]==nums[j+1]){
+                    j++;
+                 }
+             }
+             else{
+                   mp.put(nums[j],j);
+             }
+         }
      
-        for(int i=0;i<nums.length-2;i++){
-            int tar=-nums[i];
-            HashMap<Integer,Integer>mp=new HashMap<>();
-            if(i>0 && nums[i]==nums[i-1])continue;
-            for(int j=i+1;j<nums.length;j++){
-              int com=tar-nums[j];
-              
-                if(mp.containsKey(com)){
-                       ans.add(Arrays.asList(nums[i], nums[j], com));
-                      while(j<nums.length-1 && nums[j]==nums[j+1]){
-                          j++;
-                      } 
-                }
-                mp.put(nums[j],1);
-            }
-        }
+     }
         return ans;
     }
 }
