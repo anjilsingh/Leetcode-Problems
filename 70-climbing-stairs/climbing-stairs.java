79f0f95ba3de0c7d935bbcc990eb045077e92539
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-14 21:13:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (6.42%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,27 +1,60 @@
 class Solution {
     public int climbStairs(int n) {
-     int[] qb=new int[n+1];
-     int ans=countPaths(n,qb);
-     return ans;
+        //let do it by using tabulaziation method 
         
-    }
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+           if(i==1){
+             dp[i]=dp[i-1];
+           }
+            else{
+               dp[i]=dp[i-1]+dp[i-2];
+            }
+        }
+        
+        return dp[n];
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//      int[] qb=new int[n+1];
+//      int ans=countPaths(n,qb);
+//      return ans;
+        
+//     }
     
-    public int countPaths(int n,int[] qb){
-       if(n==0){
-           return 1;
-        }
-        else if(n<0){
-          return 0;
-        }
-        if(qb[n]>0){
-           return qb[n];
-        }
+//     public int countPaths(int n,int[] qb){
+//        if(n==0){
+//            return 1;
+//         }
+//         else if(n<0){
+//           return 0;
+//         }
+//         if(qb[n]>0){
+//            return qb[n];
+//         }
         
-        int m1=countPaths(n-1,qb);
-        int m2=countPaths(n-2,qb);
+//         int m1=countPaths(n-1,qb);
+//         int m2=countPaths(n-2,qb);
         
-        int ans=m1+m2;
-        qb[n]=ans;
-        return ans;
+//         int ans=m1+m2;
+//         qb[n]=ans;
+//         return ans;
     }
 }
