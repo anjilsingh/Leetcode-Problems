aee93af4268c9961189945ade4cb61673d8b2d7b
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-17 23:25:41 +0530

    Time: 9 ms (91.77%) | Memory: 48.1 MB (78.58%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,22 +1,18 @@
-import java.util.Arrays;
-
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        Arrays.sort(g); // Sort children's greed factors
-        Arrays.sort(s); // Sort cookie sizes
-        
-        int i = 0, j = 0; // i for children, j for cookies
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0, j = 0;
         int count = 0;
-        
-        // Loop through both arrays to maximize the number of content children
+
         while (i < g.length && j < s.length) {
-            if (s[j] >= g[i]) { // If cookie can satisfy child
-                count++;        // Increment count of content children
-                i++;            // Move to the next child
+            if (g[i] <= s[j]) {
+                count++;
+                i++;
             }
-            j++;                // Move to the next cookie
+            j++;
         }
-        
-        return count; // Return the number of content children
+
+        return count;
     }
 }
