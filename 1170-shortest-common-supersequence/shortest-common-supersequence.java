0dc7c98ef9199de5d31cfbbc944091b570d645f6
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-18 21:24:42 +0530

    Time: 29 ms (23.00%) | Memory: 50.1 MB (65.99%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -1,53 +1,51 @@
 class Solution {
     
     public String shortestCommonSupersequence(String str1, String str2) {
-        int n = str1.length();
-        int m = str2.length();
-        int dp[][] = new int[n+1][m+1];
-
-        // Build the DP table for Longest Common Subsequence (LCS)
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                } else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
+        int n=str1.length();
+        int m=str2.length();
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+           for(int j=0;j<=m;j++){
+            if(i==0 ||j==0){
+             dp[i][j]=0;
             }
-        }
-
-        // Build the shortest common supersequence using the DP table
-        int i = n, j = m;
-        StringBuilder result = new StringBuilder();
-
-        while (i > 0 && j > 0) {
-            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                result.append(str1.charAt(i - 1));
-                i--;
+               else if(str1.charAt(i-1)==str2.charAt(j-1)){
+                dp[i][j]=1+dp[i-1][j-1];
+             }
+               else{
+                  dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+               }
+           }
+       }
+        
+     int i=n;
+     int j=m;
+     String str="";
+     while(i>0 && j>0){
+             if(str1.charAt(i-1)==str2.charAt(j-1)){
+                 str=str+str1.charAt(i-1);
+                 i--;
+                 j--;
+             }
+             else if(dp[i-1][j]>=dp[i][j-1]){
+               str= str+str1.charAt(i-1);
+                 i--;
+             }
+             else if(dp[i][j-1]>dp[i-1][j]){
+               str=str+str2.charAt(j-1);
                 j--;
-            } else if (dp[i - 1][j] >= dp[i][j - 1]) {
-                result.append(str1.charAt(i - 1));
-                i--;
-            } else {
-                result.append(str2.charAt(j - 1));
-                j--;
-            }
-        }
-
-        // Add the remaining characters of str1 and str2
-        while (i > 0) {
-            result.append(str1.charAt(i - 1));
-            i--;
-        }
-
-        while (j > 0) {
-            result.append(str2.charAt(j - 1));
+             }
+     }
+    while(i>0){
+         str=str+str1.charAt(i-1);
+         i--;
+    }
+        while(j>0){
+            str=str+str2.charAt(j-1);
             j--;
         }
-
-        // The result is built in reverse order, so we need to reverse it
-        return result.reverse().toString();
+        
+       StringBuilder reversed = new StringBuilder(str);
+        return  reversed.reverse().toString();
     }
 }
