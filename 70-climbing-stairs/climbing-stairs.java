133af94775f09e3763dcd26ad1e314e85c1b5acf
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-20 00:14:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (53.40%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,60 +1,13 @@
 class Solution {
+   
     public int climbStairs(int n) {
-        //let do it by using tabulaziation method 
-        
-        int dp[]=new int[n+1];
-        dp[0]=1;
-        for(int i=1;i<=n;i++){
-           if(i==1){
-             dp[i]=dp[i-1];
-           }
-            else{
-               dp[i]=dp[i-1]+dp[i-2];
-            }
-        }
-        
-        return dp[n];
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//      int[] qb=new int[n+1];
-//      int ans=countPaths(n,qb);
-//      return ans;
-        
-//     }
-    
-//     public int countPaths(int n,int[] qb){
-//        if(n==0){
-//            return 1;
-//         }
-//         else if(n<0){
-//           return 0;
-//         }
-//         if(qb[n]>0){
-//            return qb[n];
-//         }
-        
-//         int m1=countPaths(n-1,qb);
-//         int m2=countPaths(n-2,qb);
-        
-//         int ans=m1+m2;
-//         qb[n]=ans;
-//         return ans;
+     int[] dp=new int[n+1];
+     dp[n]=1;
+     dp[n-1]=1;
+     for(int i=n-2;i>=0;i--){
+         dp[i]=dp[i+1]+dp[i+2];
+     }
+     return dp[0];
+      
     }
 }
