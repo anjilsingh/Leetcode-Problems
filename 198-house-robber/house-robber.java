2a956f1e153666b35115e3d48b630e0ea01c9078
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-20 20:40:03 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (63.06%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,29 +1,19 @@
 class Solution {
-    public int solve(int n, int i, int[] nums,int[] dp) {
-        // Base case: If we've gone beyond the array length, return 0
-        if (i >= n) {
-            return 0;
-        }
-
-        // Recursive case: Either take the current element and move to i+2, or skip the current element
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int max_amount = Math.max(nums[i] + solve(n, i + 2, nums,dp), solve(n, i + 1, nums,dp));
-
-        return dp[i]=max_amount;
+    public int solve(int n,int i,int[] nums,int[]dp){
+      if(i>=n){
+          return 0;
+      }
+     if(dp[i]!=-1){
+        return dp[i];
+     }
+     int ans=Math.max(nums[i]+solve(n,i+2,nums,dp),solve(n,i+1,nums,dp));
+     return dp[i]=ans;
     }
-
     public int rob(int[] nums) {
-        int n = nums.length;
-        // Edge case: If there are no houses, return 0
-        if (n == 0) {
-            return 0;
-        }
+        int n=nums.length;
         int[] dp=new int[n];
         Arrays.fill(dp,-1);
-
-        // Start solving from the first house
-        return solve(n, 0, nums,dp);
+        int ans=solve(n,0,nums,dp);
+        return ans;
     }
 }
