c20c087ded9b77155881c5924bae451448f0b42b
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-20 23:41:33 +0530

    Time: 2 ms (51.84%) | Memory: 47.3 MB (67.13%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,38 +1,37 @@
-import java.util.ArrayList;
-
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int v = isConnected.length;
-        ArrayList<ArrayList<Integer>> adjLs = new ArrayList<>();
-        for (int i = 0; i < v; i++) {
-            adjLs.add(new ArrayList<Integer>());
-        }
-        for (int i = 0; i < v; i++) {
-            for (int j = 0; j < v; j++) {
-                if (isConnected[i][j] == 1 && i != j) {
-                    adjLs.get(i).add(j);
-                    adjLs.get(j).add(i);
-                }
+    
+      public void dfs(int curr,ArrayList<ArrayList<Integer>>adj,boolean[] vis){
+        
+        vis[curr]=true;
+        for(int neg:adj.get(curr)){
+            if(!vis[neg]){
+                dfs(neg,adj,vis);
             }
         }
-
-        int[] vis = new int[v];
-        int cnt = 0;
-        for (int i = 0; i < v; i++) {
-            if (vis[i] == 0) {
-                cnt++;
-                dfs(i, adjLs, vis);
-            }
-        }
-        return cnt;
+        
     }
-
-    private static void dfs(int node, ArrayList<ArrayList<Integer>> adjLs, int[] vis) {
-        vis[node] = 1;
-        for (Integer it : adjLs.get(node)) {
-            if (vis[it] == 0) {
-                dfs(it, adjLs, vis);
-            }
+    public int findCircleNum(int[][] isConnected) {
+        ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
+        int V=isConnected.length;
+        for(int i=0;i<V;i++){
+           adj.add(new ArrayList<>());
         }
+        for(int i=0;i<V;i++){
+          for(int j=0;j<V;j++){
+             if(isConnected[i][j]==1 && i!=j){
+                adj.get(i).add(j);
+             }
+          }
+        }
+        int count=0;
+        boolean[] vis=new boolean[V];
+        for(int i=0;i<V;i++){
+           if(!vis[i]){
+              dfs(i,adj,vis);
+              count++;
+           }
+        }
+        
+        return count;
     }
 }
