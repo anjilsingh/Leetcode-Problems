ab7f3ee3fb3f1ed01bd800a704c753f23aa96a7e
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-21 10:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (68.86%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,4 +1,20 @@
 class Solution {
+    public  void dfs(int[]dx, int[] dy,int inicolor,int color,int[][] image,int sr,int sc,int r,int c){
+        
+        image[sr][sc]=color;
+        for(int j=0;j<4;j++){
+
+              int newR=sr+dx[j];
+              int newC=sc+dy[j];
+               
+               if(newR<0 ||newR>=r ||newC<0 ||newC>=c||image[newR][newC]!=inicolor){
+                 continue;
+               }
+              dfs(dx,dy,inicolor,color,image,newR,newC,r,c);
+        
+        }
+    
+    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int r=image.length;
         int c=image[0].length;
@@ -6,28 +22,11 @@ class Solution {
          if (inicolor == color) {
             return image;
         }
-        Queue<int[]>q=new LinkedList<>();
-        image[sr][sc]=color;
-        q.add(new int[]{sr,sc});
         int[] dx={-1,1,0,0};
         int[] dy={0,0,-1,1};
-        while(!q.isEmpty()){
-           int[] node=q.poll();
-           int row=node[0];
-           int col=node[1];
-           for(int j=0;j<4;j++){
-              int newR=row+dx[j];
-              int newC=col+dy[j];
-               
-               if(newR<0 ||newR>=r ||newC<0 ||newC>=c||image[newR][newC]!=inicolor){
-                 continue;
-               }
-               image[newR][newC]=color;
-               q.add(new int[]{newR,newC});
-               
-           }
-            
-        }
+        dfs(dx,dy,inicolor,color,image,sr,sc,r,c);
         return image;
+        
+        
     }
 }
