6ef0d1a84edde05516a644b7f410183b2a0ab990
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-21 09:52:25 +0530

    Time: 1 ms (46.11%) | Memory: 44.5 MB (95.47%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,30 +1,33 @@
 class Solution {
-    private int[][] dfs(int[][] image,int[][] ans,int sr,int sc,int[] dx, int[] dy,int color,int iniColor){
-         int n=image.length;
-         int m=image[0].length;
-        ans[sr][sc]=color;
-        for(int i=0;i<4;i++){
-           int  newSr=sr+dx[i];
-           int  newSc=sc+dy[i];
-            
-            
-            if(newSr>=0 && newSr<n && newSc>=0 && newSc<m && ans[newSr][newSc]!=color && image[newSr][newSc]==iniColor){
-                ans[newSr][newSc]=color;
-                dfs(image,ans,newSr,newSc,dx,dy,color,iniColor);
-
-            }
-        }
-        return ans;
-        
-   }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        //using recursion can be done using bfs or dfs
-        
-        int iniColor=image[sr][sc];
-        int[][] ans=image;
+        int r=image.length;
+        int c=image[0].length;
+        int inicolor=image[sr][sc];
+         if (inicolor == color) {
+            return image;
+        }
+        Queue<int[]>q=new LinkedList<>();
+        image[sr][sc]=color;
+        q.add(new int[]{sr,sc});
         int[] dx={-1,1,0,0};
         int[] dy={0,0,-1,1};
-        ans=dfs(image,ans,sr,sc,dx,dy,color,iniColor);
-        return ans;
+        while(!q.isEmpty()){
+           int[] node=q.poll();
+           int row=node[0];
+           int col=node[1];
+           for(int j=0;j<4;j++){
+              int newR=row+dx[j];
+              int newC=col+dy[j];
+               
+               if(newR<0 ||newR>=r ||newC<0 ||newC>=c||image[newR][newC]!=inicolor){
+                 continue;
+               }
+               image[newR][newC]=color;
+               q.add(new int[]{newR,newC});
+               
+           }
+            
+        }
+        return image;
     }
 }
