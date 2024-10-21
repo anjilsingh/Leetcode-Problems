2ea5f4a1f210cea1a62cd254131c809f38932946
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-21 16:31:47 +0530

    Time: 14 ms (67.99%) | Memory: 46.9 MB (79.85%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,4 +1,5 @@
-class Node {
+class Solution {
+    class Node {
     int first;
     int second;
     int third; 
@@ -8,55 +9,47 @@ class Node {
         this.third = _third; 
     }
 }
-
-class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int n=mat.length;
-        int m=mat[0].length;
-        int vis[][]=new int[n][m];
-        int dist[][]=new int[n][m];
+        int row=mat.length;
+        int col=mat[0].length;
+       int[][]dis=new int[row][col];
+       int[][]vis=new int[row][col];
         
-        Queue<Node>q=new LinkedList<>();
-        for(int i=0;i<n;i++){
-
-           for(int j=0;j<m;j++){
+       Queue<Node>q=new LinkedList<>();
+        for(int i=0;i<row;i++){
+           for(int j=0;j<col;j++){
+            
                if(mat[i][j]==0){
-                   q.add(new Node(i,j,0));
-                   vis[i][j]=1;
-               }
+                  dis[i][j]=0;
+                 q.add(new Node(i,j,0));
+                 vis[i][j]=1;
+             
+              }
                else{
-                  vis[i][j]=0;
+                vis[i][j]=0;
                }
            }
         }
+        int[] dx={-1,1,0,0};
+        int[] dy={0,0,-1,1};
         while(!q.isEmpty()){
             
-        int nn=q.peek().first;
-        int mm=q.peek().second;
-        int step=q.peek().third;
-         q.poll(); 
-        dist[nn][mm]=step;
-        int[] delrow={-1,1,0,0};
-        int[] delcol={0,0,-1,1};
-        
-        for(int i=0;i<4;i++){
-              int row=delrow[i]+nn;
-             int col=delcol[i]+mm;
-            
-            if(row>=0 && row<n && col>=0 && col<m && vis[row][col]==0){
-
-               q.add(new Node(row,col,step+1));
-                
-                vis[row][col]=1;
-            }
-        
-        
+              int r=q.peek().first;
+              int c=q.peek().second;
+              int steps=q.peek().third;
+              q.remove();
+              dis[r][c]=steps;
+              for(int j=0;j<4;j++){
+                  int newr=r+dx[j];
+                  int newc=c+dy[j];
+         if(newr<0 ||newr>=row ||newc<0 ||newc>=col||vis[newr][newc]==1)continue;
+                  
+                  q.add(new Node(newr,newc,steps+1));
+                  vis[newr][newc]=1;
+              }
         }
-        
-        } 
-        return dist;
-        
-      
-        
+        return dis;
+
+    
     }
 }
