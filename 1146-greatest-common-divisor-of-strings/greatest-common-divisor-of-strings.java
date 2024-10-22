da1064e16f59051733e1fb1043eb5d90ce32899f
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-22 09:38:23 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (63.69%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -1,13 +1,24 @@
 class Solution {
       public String gcdOfStrings(String str1, String str2) {
-        if (str1.equals(str2))
+          //logic base case since it has gcd at one time there length must be equal 
+          
+          if(str1.equals(str2)){
             return str1;
-
-        String strMin = str1, strMax = str2;
-        if (str1.length() > str2.length()) {
-            strMin = str2; strMax = str1;
-        }
-        if (strMax.indexOf(strMin) != 0) return "";
-        return gcdOfStrings(strMin, strMax.substring(strMin.length()));
+          }
+          String maxString=str1;
+          String minString=str2;
+          
+          if(str2.length()>str1.length()){
+              maxString=str2;
+              minString=str1;
+              
+          }
+          
+          if(maxString.indexOf(minString)!=0){
+            return "";
+          }
+          
+          return gcdOfStrings(minString,maxString.substring(minString.length()));
+          
 }
 }
