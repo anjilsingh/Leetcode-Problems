7c4284acc6366ab042c835160f53300d5da152ab
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-23 09:51:55 +0530

    Time: 1 ms (62.65%) | Memory: 45 MB (52.87%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,36 +1,32 @@
 class Solution {
-    public boolean bfs(int curr,int[][]graph,int[] color){
-
-    Queue<Integer>q=new LinkedList<>();
-    q.add(curr);
-    color[curr]=1;
-    while(!q.isEmpty()){
-        int node=q.poll();
-         for(int neg:graph[node]){
-            if(color[neg]==-1){
-                 q.add(neg);
+    public boolean bfs(int curr,int[] color,int[][]graph){
+        Queue<Integer>q=new LinkedList<>();
+        q.add(curr);
+        color[curr]=1;
+        while(!q.isEmpty()){
+           int node=q.poll();
+          for(int neg:graph[node]){
+              if(color[neg]==-1){
+                q.add(neg);
                 color[neg]=1-color[node];
-            }
-            else if(color[neg]==color[node]){
-               return false;
-            }
-         }
-    }
+              }
+              else if(color[neg]==color[node]){
+                 return false;
+              }
+
+          }
+            
+        }
         return true;
-    
-    
     }
     public boolean isBipartite(int[][] graph) {
-   //the graph will ne bipartiate if we can color its nodes such that two adjacent nodes do not contains same color
-       int V=graph.length;
-       int[] color=new int[V];
-       Arrays.fill(color,-1);
-     
+        int V=graph.length;
+        int[] color=new int[V];
+        Arrays.fill(color,-1);
         for(int i=0;i<V;i++){
-          if(color[i]==-1){
-             if(bfs(i,graph,color)==false) return false;
-              
-          }
+             if(color[i]==-1){
+               if(bfs(i,color,graph)==false) return false;
+             }
         }
         return true;
     }
