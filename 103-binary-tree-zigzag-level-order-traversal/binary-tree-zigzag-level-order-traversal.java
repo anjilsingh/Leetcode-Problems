ff0dd8eeda892bb1dcf0be60aadc3068e06322b9
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-10-30 16:39:40 +0530

    Time: 1 ms (71.12%) | Memory: 42.5 MB (8.87%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -13,38 +13,37 @@
  *     }
  * }
  */
-public class Solution {
+class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         if (root == null) return ans;
 
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        boolean leftToRight = true;
+        boolean flag = true; // true means left-to-right, false means right-to-left
 
         while (!q.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
             int size = q.size();
-            List<Integer> level = new LinkedList<>();
 
             for (int i = 0; i < size; i++) {
                 TreeNode node = q.poll();
-
-                if (leftToRight) {
-                    level.add(node.val);
-                } else {
-                    level.add(0, node.val); // Add to the beginning for reverse order
-                }
-
-                if (node.left != null) {
-                    q.add(node.left);
-                }
-                if (node.right != null) {
-                    q.add(node.right);
-                }
+                
+                // Simply add values to `temp`
+                temp.add(node.val);
+                
+                // Add children to queue
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
             }
 
-            ans.add(level);
-            leftToRight = !leftToRight; // Toggle the direction
+            // Reverse `temp` if flag is false for right-to-left order
+            if (!flag) {
+                Collections.reverse(temp);
+            }
+            
+            ans.add(temp);
+            flag = !flag; // Toggle flag for next level
         }
 
         return ans;
