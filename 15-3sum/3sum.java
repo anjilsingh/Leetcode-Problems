12757e66be5d8baadcfbfbede9db2e9442d5648c
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-11-02 08:06:27 +0530

    Time: 30 ms (67.36%) | Memory: 51.2 MB (75.22%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,27 +1,29 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
       List<List<Integer>>ans=new ArrayList<>();
-      Arrays.sort(nums);
-      int n=nums.length;
-     for(int i=0;i<n-2;i++){
-        if(i>0 && nums[i]==nums[i-1]) continue;
-         HashMap<Integer,Integer>mp=new HashMap<>();
-         int x=-nums[i];
-         
-         for(int j=i+1;j<n;j++){
-             int tar=x-nums[j];
-             if(mp.containsKey(tar)){
-                ans.add(Arrays.asList(nums[i],nums[j],tar));
-                 while(j+1<n && nums[j]==nums[j+1]){
-                    j++;
-                 }
+        int n=nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i<n-2;i++){
+            if(i>0 && nums[i-1]==nums[i]) continue;
+            int l=i+1;
+            int r=n-1;
+            int target=-nums[i];
+            while(l<r){
+             if(nums[l]+nums[r]==target){
+                 ans.add(Arrays.asList(nums[i],nums[l] , nums[r]));
+                 while(l<r && nums[l]==nums[l+1])l++;
+                 while(r>l && nums[r]==nums[r-1])r--;
+                 l++;
+                 r--;
+             }
+             else if(nums[l]+nums[r]<target){
+                 l++;
              }
              else{
-                   mp.put(nums[j],j);
+                 r--;
              }
-         }
-     
-     }
+            }
+        }
         return ans;
     }
 }
