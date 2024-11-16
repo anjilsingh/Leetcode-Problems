22ee6ed91190cf1443098e5c981ff3a66c2fda9d
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-11-16 11:47:48 +0530

    Time: 2 ms (97.22%) | Memory: 41.4 MB (70.06%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,29 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
-       Stack<Character>st=new Stack<>();
-       for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
-              st.push(s.charAt(i));
+        Stack<Character>st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
             }
-           else{
-               if(st.isEmpty()){
-                return false;
-               }
-               char x=st.peek();
-               
-if ((s.charAt(i) == ')' && x == '(') || 
-    (s.charAt(i) == '}' && x == '{') || 
-    (s.charAt(i) == ']' && x == '['))
+     else if ((ch == ')' && !st.isEmpty() && st.peek() == '(') || 
+                       (ch == '}' && !st.isEmpty() && st.peek() == '{') || 
+                       (ch == ']' && !st.isEmpty() && st.peek() == '[')) {
+                st.pop();  // Pop the matching opening bracket from the stack
+            }
+            else{
+                 return false;
+            }
 
-{
-       st.pop();
-}
-               else{
-               return false;
-               }
-           }
-          
-       }
-     return st.isEmpty();
+        }
+        return st.isEmpty();
     }
 }
