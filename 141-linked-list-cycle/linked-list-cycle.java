21da738df732cf7917552c6e3507fe9c1144db41
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-11-16 14:42:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (21.10%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -9,22 +9,16 @@
  *     }
  * }
  */
-import java.util.HashMap;
 public class Solution {
     public boolean hasCycle(ListNode head) {
-      if(head==null ||head.next==null){
-          return false;
-        }
-       ListNode slow=head;
-       ListNode fast=head;
+        if(head==null || head.next==null) return false;
+        ListNode slow=head;
+        ListNode fast=head;
         while(fast!=null && fast.next!=null){
-          fast=fast.next.next;
-          slow=slow.next;
-            if(slow==fast){
-               return true;
-            }
+            fast=fast.next.next;
+            slow=slow.next;
+            if(slow==fast) return true;
         }
-        
         return false;
     }
 }
