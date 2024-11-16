d7e568f77394952575065f8dfecbbf10924b0756
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-11-16 21:58:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (13.33%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -14,16 +14,9 @@
  * }
  */
 class Solution {
-  public boolean isSameTree(TreeNode p, TreeNode q) 
-  {
-      if(p==null && q==null){
-             return true;
-      }
-      if(p==null ||q==null){
-
-      return false;}
-      return ((p.val==q.val) && isSameTree(p.left,q.left) && isSameTree(p.right,q.right));
-      
-      
-  }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        if(p==null||q==null) return false;
+        return (p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right));
+    }
 }
