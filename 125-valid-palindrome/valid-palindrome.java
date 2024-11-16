8cd0c5acd731c0f749085c7118cdbdae9388f88b
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-11-16 10:09:44 +0530

    Time: 3 ms (72.40%) | Memory: 42.6 MB (95.00%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,44 +1,30 @@
-import java.util.*;
-
 class Solution {
-    // Reverse a string using char array
-    public String reverse(String str) {
-        char[] r = str.toCharArray();
-        int l = 0;
-        int m = str.length() - 1;
-        
-        // Perform in-place swapping of characters
-        while (l <= m) {
-            char temp = r[l];
-            r[l] = r[m];
-            r[m] = temp;
-            l++;
-            m--;
+    public boolean isPalindrome(String s) {
+        int low=0;
+        int high=s.length()-1;
+        while(low<high){
+            
+            char lowchar=Character.toLowerCase(s.charAt(low));
+            char highchar=Character.toLowerCase(s.charAt(high));
+         if(Character.isLetterOrDigit(lowchar) && Character.isLetterOrDigit(highchar)){
+               if(lowchar!=highchar) return false;
+                else{
+                  low++;
+                  high--;
+
+                }
+            }
+            
+            else if(Character.isLetterOrDigit(lowchar)){
+                high--;
+
+            }
+            else{
+                  low++;
+            }
+
         }
         
-        // Convert the char array back to a String
-        return new String(r);
-    }
-
-    // Check if a string is a palindrome
-    public boolean isPalindrome(String s) {
-        // Remove all non-alphanumeric characters and convert to lowercase
-        String str = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        // Reverse the cleaned string
-        String rev = reverse(str);
-
-        // Check if the original cleaned string is equal to the reversed string
-        return str.equals(rev);
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        String input = "A man, a plan, a canal: Panama";
-        
-        // Check if the string is a palindrome
-        boolean result = solution.isPalindrome(input);
-        
-        System.out.println(result); // Output: true
+        return true;
     }
 }
