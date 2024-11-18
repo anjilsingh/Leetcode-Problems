e4b69129521e2553f9ccecd8041e656cbafbff9f
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-11-18 10:47:12 +0530

    Time: 4 ms (76.24%) | Memory: 42.9 MB (80.80%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,12 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        char[] sarr=s.toCharArray();
-        char[] tarr=t.toCharArray();
-        Arrays.sort(sarr);
-        Arrays.sort(tarr);
-        for(int i=0;i<sarr.length;i++){
-           if(sarr[i]!=tarr[i]) return false;
+        //second method can use an int array 
+        //increase value for char in s and decrease for t if all are 0 then okay else return false
+        //since only lower case letter so use 26 length array
+        
+        int[] arr=new int[26];
+        for(int i=0;i<s.length();i++){
+           arr[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+             arr[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+           if(arr[i]!=0) return false;
         }
         return true;
     }
