abbe2d6e3eed58424d322428fd0587b70b6545b3
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-11-19 10:22:32 +0530

    Time: 2 ms (88.74%) | Memory: 55.3 MB (66.55%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,31 +1,21 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        //lets see the third approach of prefix and suffix multiplication
-        int[] ans=new int[nums.length];
-        int len=nums.length;
-        int prefix[]=new int[len];
-        int suffix[]=new int[len];
-        //lets initialize prefix array
-        prefix[0]=nums[0];
-        for(int i=1;i<len;i++){
-            prefix[i]=prefix[i-1]*nums[i];
+        int n=nums.length;
+        int[] ans=new int[n];
+        //first we keep multiply our answer from front side
+        ans[0]=1;
+        for(int i=1;i<n;i++){
+             ans[i]=ans[i-1]*nums[i-1];
         }
-        
-        //lets initialize suffix array
-        suffix[len-1]=nums[len-1];
-        for(int i=len-2;i>=0;i--){
-            suffix[i]=suffix[i+1]*nums[i];
-        }
-        
-        for(int i=0;i<len;i++){
-            if(i==0){
-              ans[i]=suffix[i+1];
-            }
-            else if(i==len-1){
-                  ans[i]=prefix[len-2];
+        int post=1;
+        for(int i=n-1;i>=0;i--){
+            if(i==n-1){
+               ans[i]=ans[i]*post;
+               post=nums[i]*post;
             }
             else{
-            ans[i]=prefix[i-1]*suffix[i+1];
+              ans[i]=ans[i]*post;
+              post=post*nums[i];
             }
         }
         return ans;
