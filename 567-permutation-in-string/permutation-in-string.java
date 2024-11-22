ead1c146caf6a295e60d59c6945fdc933eb885af
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-11-22 20:14:35 +0530

    Time: 7 ms (72.72%) | Memory: 42.5 MB (75.06%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,41 +1,35 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
+        if(s1.length()>s2.length()) return false;
+        int s1count[]=new int[26];
+        int s2count[]=new int[26];
+        for(int i=0;i<s1.length();i++){
+            s1count[s1.charAt(i)-'a']++;
+            s2count[s2.charAt(i)-'a']++;
+            
+        }
+        int matches=0;
+        for(int i=0;i<26;i++){
+           if(s1count[i]==s2count[i]) matches++;
+        }
+        int l=0;
+        for(int r=s1.length();r<s2.length();r++){
+            if(matches==26) return true;
+            //since we have added r
+            int index=s2.charAt(r)-'a';
+            s2count[index]++;
+            if(s1count[index]==s2count[index]) matches++;
+            else if(s1count[index]+1==s2count[index]) matches--;
+            
+            //now we need to remove from l side
+            index=s2.charAt(l)-'a';
+            s2count[index]--;
+            if(s1count[index]==s2count[index]) matches++;
+            else if(s1count[index]==s2count[index]+1)matches--;
+            
+            l++;
+        }
+        return matches==26;
         
-        if (m > n) return false;  // If s1 is longer than s2, no permutation is possible.
-
-        // Frequency arrays for s1 and the current window in s2
-        int[] s1Freq = new int[26];
-        int[] windowFreq = new int[26];
-
-        // Count frequency array for s1
-        for (char c : s1.toCharArray()) {
-            s1Freq[c - 'a']++;
-        }
-
-        // Initialize the first window of size m in s2
-        for (int i = 0; i < m; i++) {
-            windowFreq[s2.charAt(i) - 'a']++;
-        }
-
-        // Slide the window over the string s2
-        for (int i = 0; i <= n - m; i++) {
-            // If frequencies match, we found a valid permutation
-            if (Arrays.equals(s1Freq, windowFreq)) {
-                return true;
-            }
-
-            // Slide the window to the right:
-            if (i + m < n) {
-                // Remove the first character of the current window
-                windowFreq[s2.charAt(i) - 'a']--;
-                // Add the next character to the window
-                windowFreq[s2.charAt(i + m) - 'a']++;
-            }
-        }
-
-        // Check the last window
-        return Arrays.equals(s1Freq, windowFreq);
     }
 }
