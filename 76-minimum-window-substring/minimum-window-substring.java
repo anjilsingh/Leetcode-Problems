abd8a6ae33132821d09718d255f0e56d5286dbfb
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-11-23 19:24:27 +0530

    Time: 21 ms (38.36%) | Memory: 45.5 MB (19.05%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,51 +1,59 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length() || t.length() == 0) return "";
-        if (s.equals(t)) return s;
-
-        int[] ind = {-1, -1};
-
-        // Map to store character counts for `t`
-        HashMap<Character, Integer> requiredCounts = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            requiredCounts.put(c, requiredCounts.getOrDefault(c, 0) + 1);
+        if(t.length()==0) return "";
+        
+        HashMap<Character,Integer>tmp=new HashMap<>();
+        HashMap<Character,Integer>smp=new HashMap<>();
+        
+        for(char c:t.toCharArray()){
+              tmp.put(c,tmp.getOrDefault(c,0)+1);
         }
-
-        int need = requiredCounts.size(); // Unique characters in `t`
-        int have = 0;
-        HashMap<Character, Integer> windowCounts = new HashMap<>();
-        int l = 0;
-
-        for (int r = 0; r < s.length(); r++) {
-            // Add character to the window
-            char curr = s.charAt(r);
-            windowCounts.put(curr, windowCounts.getOrDefault(curr, 0) + 1);
-
-            // Check if the current character satisfies a requirement
-            if (requiredCounts.containsKey(curr) && windowCounts.get(curr).intValue() == requiredCounts.get(curr).intValue()) {
-                have++;
+        int need=tmp.size();
+        
+        int ind[]={-1,-1};
+        int have=0;
+        int l=0;
+        for(int r=0;r<s.length();r++){
+            char ch=s.charAt(r);
+        
+            
+          if (tmp.containsKey(ch)) {
+                smp.put(ch, smp.getOrDefault(ch, 0) + 1);
+                if (smp.get(ch).equals(tmp.get(ch))) {
+                    have++;
+                }
             }
 
-            // Shrink the window while it contains all required characters
-            while (have == need) {
-                // Update result if the current window is smaller
-                if (ind[0] == -1 || r - l + 1 < ind[1] - ind[0] + 1) {
-                    ind[0] = l;
-                    ind[1] = r;
-                }
+            while(have==need){
+               
+                if(ind[0]==-1 || (ind[1]-ind[0]+1)>=r-l+1){
 
-                // Remove the leftmost character from the window
-                char leftChar = s.charAt(l);
-                windowCounts.put(leftChar, windowCounts.get(leftChar) - 1);
-                if (requiredCounts.containsKey(leftChar) && windowCounts.get(leftChar) < requiredCounts.get(leftChar)) {
-                    have--;
+                 ind[0]=l;
+                 ind[1]=r;
                 }
+                
+                //now remove the character from l pointer side
+                
+                char c=s.charAt(l);
+               
+                
+     if (tmp.containsKey(c)) {
+                    smp.put(c, smp.get(c) - 1);
+                    if (smp.get(c) < tmp.get(c)) {
+                        have--;
+                    }
+                    if (smp.get(c) == 0) {
+                        smp.remove(c);
+                    }
+                }
+                l++;
 
-                l++; // Move the left pointer
             }
-        }
 
-        if (ind[0] == -1) return ""; // No valid window found
-        return s.substring(ind[0], ind[1] + 1); // Return the smallest window
+        
+        }
+        
+        if(ind[0]==-1) return "";
+        return s.substring(ind[0],ind[1]+1);
     }
 }
