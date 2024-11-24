6764e0d3ee413b66ba3f7275de2a888a0008551e
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-11-24 21:13:56 +0530

    Time: 94 ms (7.74%) | Memory: 44.9 MB (26.18%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,30 +1,16 @@
 class Solution {
-    // public static boolean isUnique(String str){
-    //  HashSet<Character>seen=new HashSet<>();
-    //     for(char c:str.toCharArray()){
-    //       if(seen.contains(c)) return false;
-    //         seen.add(c);
-    //     }
-    //     return true;
-    // }
     public int lengthOfLongestSubstring(String s) {
-        
-        //bruteforce approach 
-        //first i will do the most basic approach
-        int len=0;
+        int maxLength=0;
         for(int i=0;i<s.length();i++){
             HashSet<Character>seen=new HashSet<>();
-         for(int j=i;j<s.length();j++){
-             if(seen.contains(s.charAt(j))){
-              break;
-             }
-             else{
-                 len=Math.max(len,j-i+1);
-                 seen.add(s.charAt(j));
-             }
-         
-         }
+            for(int j=i;j<s.length();j++){
+              if(seen.contains(s.charAt(j))) break;
+              else{
+               maxLength=Math.max(maxLength,j-i+1);
+               seen.add(s.charAt(j));
+              }
+            }
         }
-        return len;
+        return maxLength;
     }
 }
