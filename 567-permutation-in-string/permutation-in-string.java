f8e10130f13dd178cf171420dc18c17f4d2267dd
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-11-24 22:27:39 +0530

    Time: 9 ms (47.53%) | Memory: 42.5 MB (75.08%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,33 +1,43 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        //lets see the brute force approach
-        //the idea is to find all substring of size s1 in s2 and then sort both and 
-        //cmpare if true return immediatey treu 
-        //otherwise compare all substring and finally if not matched return 
-        //false
-        
-        //hahaha done:)
+        //lets do it more optimized :)
+        //using sliding window protocol
+        //this is a fixed winow size problem 
+        //so first we compute for s1 length and 
+        //then compute further by removing one character and adding one 
         
         if(s2.length()<s1.length()) return false;
-        if(s2.equals(s1)) return true;
+        if(s1.equals(s2)) return true;
         
-        char[] s1char=s1.toCharArray();
-        Arrays.sort(s1char);
-        String sortedS1=new String(s1char);
+        int[] s1arr=new int[26];
+        int[] s2arr=new int[26];
         
-        
-        for(int i=0;i<s2.length();i++){
-          for(int j=i;j<s2.length();j++){
-            if(j-i+1==s1.length()){
-
-             char[] s2char=s2.substring(i,j+1).toCharArray();
-             Arrays.sort(s2char);
-             String sorteds2=new String(s2char);
-            if(sortedS1.equals(sorteds2)) return true;
-            }
-          }
+        for(int i=0;i<s1.length();i++){
+            s1arr[s1.charAt(i)-'a']++;
+            s2arr[s2.charAt(i)-'a']++;
         
         }
-        return false;
+        int matches=0;
+        for(int i=0;i<26;i++){
+         if(s1arr[i]==s2arr[i])  matches++;
+        }
+         int l=0;
+        for(int r=s1.length();r<s2.length();r++){
+            if(matches==26) return true;
+            
+            int index=s2.charAt(r)-'a';
+            s2arr[index]++;
+            if(s1arr[index]==s2arr[index]) matches++;
+            else if(s1arr[index]+1==s2arr[index]) matches--;
+            
+            //now remove from l side
+             index=s2.charAt(l)-'a';
+            s2arr[index]--;
+            if(s1arr[index]==s2arr[index]) matches++;
+            else if(s1arr[index]==s2arr[index]+1) matches--;
+            l++;
+            
+        }
+        return matches==26;
     }
 }
