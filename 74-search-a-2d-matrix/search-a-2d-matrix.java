fe51ba17e0bcd125eb8447edcbeb9cad81e01093
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-11-25 19:59:38 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (68.60%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,25 +1,16 @@
-class Solution {
+public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        //one way is to change 2d matrix in 1d and apply  binary search
-        
-       int[] oneD = new int[matrix.length * matrix[0].length];
+        int m = matrix.length, n = matrix[0].length;
+        int r = 0, c = n - 1;
 
-// Fill the 1D array
-int index = 0;
-for (int i = 0; i < matrix.length; i++) {
-    for (int j = 0; j < matrix[0].length; j++) {
-        oneD[index++] = matrix[i][j];
-    }
-}
-
-        int l=0;
-        int r=oneD.length-1;
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(oneD[mid]==target) return true;
-            else if(oneD[mid]<target) l=mid+1;
-            else r=mid-1;
-
+        while (r < m && c >= 0) {
+            if (matrix[r][c] > target) {
+                c--;
+            } else if (matrix[r][c] < target) {
+                r++;
+            } else {
+                return true;
+            }
         }
         return false;
     }
