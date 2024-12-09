99723803abd09ebed86568c73c714b6a5994d0a6
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-09 21:29:08 +0530

    Time: 16 ms (21.09%) | Memory: 48.8 MB (36.54%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,17 +1,25 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-       HashMap<Integer,Integer>mp=new HashMap<>();
-       for(int num:nums){
-              mp.put(num,mp.getOrDefault(num,0)+1);
-       }
-      List<int[]>arr=new ArrayList<>();
-     for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
-          arr.add(new int[]{entry.getValue(),entry.getKey()});
-     }
-      arr.sort((a,b)->b[0]-a[0]);
-      int[] res=new int[k];
+        //can use a hashmap 
+        //find the frq create an arralist based on this freq and
+        //then sort it with the help of freq and return the k number 
+        
+        //lets start with creating a hashmap
+        
+        HashMap<Integer,Integer>mp =new HashMap<>();
+        for(int num:nums){
+         mp.put(num,mp.getOrDefault(num,0)+1);
+         
+        }
+        List<int[]>arr=new ArrayList<>();
+        for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
+              arr.add(new int[]{entry.getValue(),entry.getKey()});
+        }
+        arr.sort((a,b)->b[0]-a[0]);
+        int[] res=new int[k];
         for(int i=0;i<k;i++){
-              res[i]=arr.get(i)[1];
+            res[i]=arr.get(i)[1];
+
         }
         return res;
     }
