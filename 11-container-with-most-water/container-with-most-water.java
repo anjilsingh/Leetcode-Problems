aae7587334085572a0653fc585a897aa3c679262
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-11 20:51:22 +0530

    Time: 4 ms (90.40%) | Memory: 57.9 MB (56.92%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,24 +1,15 @@
 class Solution {
     public int maxArea(int[] height) {
-        //brute force can be to check for all pair
-        
-        int maxArea=Integer.MIN_VALUE;
-        
         int l=0;
         int r=height.length-1;
-        
+        int marea=Integer.MIN_VALUE;
         while(l<r){
-            int area=Math.min(height[l],height[r])*(r-l);
-            maxArea=Math.max(area,maxArea);
-            
-            if(height[l]<=height[r]){
-                
-                l++;
-            }
-            else{
-             r--;
-            }
+              int area=Math.min(height[l],height[r])*(r-l);
+              if(area>marea) marea=area;
+              
+            if(height[l]<height[r]) l++;
+            else r--;
         }
-        return maxArea;
+        return marea;
     }
 }
