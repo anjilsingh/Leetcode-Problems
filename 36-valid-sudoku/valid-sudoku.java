d468636fe4ebaf045c41e5639e08b2f72092d461
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-11 18:36:34 +0530

    Time: 2 ms (78.39%) | Memory: 44.3 MB (64.34%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,11 +1,19 @@
+
+
+import java.util.HashSet;
+
 class Solution {
-    public boolean isValid(char[][] board, int i, int j) {
-        HashSet<Character> seen = new HashSet<>();
-        for (int r = 0; r < 3; r++) {
-            for (int c = 0; c < 3; c++) {
-                char ch = board[i + r][j + c];
-                if (ch != '.' && seen.contains(ch)) return false; // Check for duplicates
-                if (ch != '.') seen.add(ch); // Add valid numbers
+    public boolean isValid(int r, int c, char[][] board) {
+        HashSet<Character> hs = new HashSet<>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[r + i][c + j] == '.') continue;
+                else {
+                    if (hs.contains(board[r + i][c + j])) return false;
+                    else {
+                        hs.add(board[r + i][c + j]);
+                    }
+                }
             }
         }
         return true;
@@ -16,39 +24,37 @@ class Solution {
         for (int i = 0; i < 9; i++) {
             HashSet<Character> seen = new HashSet<>();
             for (int j = 0; j < 9; j++) {
-                char ch = board[i][j];
-                if (ch != '.' && seen.contains(ch)) return false;
-                if (ch != '.') seen.add(ch);
+                if (board[i][j] == '.') continue;
+                else {
+                    if (seen.contains(board[i][j])) return false;
+                    else {
+                        seen.add(board[i][j]);
+                    }
+                }
             }
         }
 
         // Check all columns
         for (int i = 0; i < 9; i++) {
             HashSet<Character> seen = new HashSet<>();
-            for (int j= 0; j < 9; j++) {
-                char ch = board[j][i];
-                if (ch != '.' && seen.contains(ch)) return false;
-                if (ch != '.') seen.add(ch);
+            for (int j = 0; j < 9; j++) {
+                if (board[j][i] == '.') continue;
+                else {
+                    if (seen.contains(board[j][i])) return false;
+                    else {
+                        seen.add(board[j][i]);
+                    }
+                }
             }
         }
 
-        // Check specific 3x3 subgrids (hardcoded)
-        // if (!isValid(board, 0, 0)) return false;
-        // if (!isValid(board, 0, 3)) return false;
-        // if (!isValid(board, 0, 6)) return false;
-        // if (!isValid(board, 3, 0)) return false;
-        // if (!isValid(board, 3, 3)) return false;
-        // if (!isValid(board, 3, 6)) return false;
-        // if (!isValid(board, 6, 0)) return false;
-        // if (!isValid(board, 6, 3)) return false;
-        // if (!isValid(board, 6, 6)) return false;
-        
-        for(int i=0;i<9;i=i+3){
-            for(int j=0;j<9;j=j+3){
-                 if(!isValid(board,i,j)) return false;
+        // Check all 3x3 grids
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                if (!isValid(i, j, board)) return false;
             }
         }
 
-        return true; // If all checks pass
+        return true;
     }
 }
