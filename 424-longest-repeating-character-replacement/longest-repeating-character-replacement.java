8ee4c2cfe5378435d5f6a356b5ea35f45657ca32
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-15 20:05:56 +0530

    Time: 31 ms (24.29%) | Memory: 45.3 MB (7.09%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,29 +1,23 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        //lets do it by sliding winow approach the idea is 
-        //that we will add one element in a map 
-        //and then we will remove it from left side while
-        //lenWinodw-mfreq>k
-        //after that we will update len 
-        //finallly return the final length
+        //lets do it by wliding window tech 
         
-        
-        int len=0;
         int l=0;
         HashMap<Character,Integer>mp=new HashMap<>();
         int mfreq=0;
-        
+        int len=0;
         for(int r=0;r<s.length();r++){
-          mp.put(s.charAt(r),mp.getOrDefault(s.charAt(r),0)+1);
-          mfreq=Math.max(mfreq,mp.get(s.charAt(r)));
-                 
-          while(r-l+1-mfreq>k){
-            mp.put(s.charAt(l),mp.getOrDefault(s.charAt(l),0)-1);
-            l++;
+            mp.put(s.charAt(r),mp.getOrDefault(s.charAt(r),0)+1);
             mfreq=Math.max(mfreq,mp.get(s.charAt(r)));
-          }
-          len=Math.max(len,r-l+1);
+        
+            while(r-l+1-mfreq>k){
+                mp.put(s.charAt(l),mp.get(s.charAt(l))-1);
+                l++;
+                  mfreq=Math.max(mfreq,mp.get(s.charAt(r)));
+            }
+            
+            len=Math.max(len,r-l+1);
         }
-                 return len;
+        return len; 
     }
 }
