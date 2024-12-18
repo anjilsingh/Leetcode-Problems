c5b9936eb9a084acaf9452f75f0b8fa329077637
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-18 18:47:35 +0530

    Time: 3 ms (72.36%) | Memory: 43.1 MB (60.25%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,15 +1,27 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        //can do it by removing extra spaces and then creating it using a string bbbuilder
-        // and then reverse it 
+        //lets do it by using two ;pointer
         
+        int l=0;
+        int r=s.length()-1;
         
-        StringBuilder sb=new StringBuilder();
-        for(char c:s.toCharArray()){
-            if(Character.isLetterOrDigit(c))
-           sb.append(Character.toLowerCase(c));
+        while(l<r){
+            char leftChar=Character.toLowerCase(s.charAt(l));
+            char rightChar=Character.toLowerCase(s.charAt(r));
+            
+            if(Character.isLetterOrDigit(leftChar) && Character.isLetterOrDigit(rightChar)) {
+                 if(leftChar!=rightChar) return false;
+                 l++;
+                r--;
+            }
+            else if(Character.isLetterOrDigit(leftChar)){
+              r--;
+            }
+            else{
+                l++;
+            }
         }
         
-        return sb.toString().equals(sb.reverse().toString());
+        return true;
     }
 }
