f62ed70ea2db6ea65e51b7d2c75622928ada31a3
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-18 13:51:16 +0530

    Time: 3 ms (90.24%) | Memory: 45.1 MB (7.89%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //finally using hashtable
-        if(s.length()!=t.length()) return false;
-        int[] arr=new int[26];
-        for(int i=0;i<s.length();i++){
-            arr[s.charAt(i)-'a']++;
-        }
-        for(int i=0;i<t.length();i++){
-           arr[t.charAt(i)-'a']--;
-        }
-        for(int i=0;i<26;i++){
-            if(arr[i]!=0) return false;
-        }
-        return true;
+        //anagram means same character 
+        //so we have two options one to 
+        //soet the strings or can use a hashtable 
+        // or hashmap
+        
+        char[] schar=s.toCharArray();
+        char[] tchar=t.toCharArray();
+        
+        Arrays.sort(schar);
+        String sorteds=new String(schar);
+        Arrays.sort(tchar);
+        String sortedt=new String(tchar);
+        return sorteds.equals(sortedt);
     }
 }
