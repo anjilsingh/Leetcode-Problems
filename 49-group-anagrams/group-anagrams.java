47d851031ff27d0c4e25eeb3271c2b9723ba2a2e
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-18 15:00:50 +0530

    Time: 18 ms (23.11%) | Memory: 49.7 MB (6.12%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,14 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         //first crate the res list of list of string
-   
+    //or instead of sorting we can use a hashtable 
+        
         HashMap<String,List<String>>mp=new HashMap<>();
         for(String str:strs){
-            char strchar[]=str.toCharArray();
-            Arrays.sort(strchar);
-            String sortedStr=new String(strchar);
-            mp.putIfAbsent(sortedStr,new ArrayList<>());
-            mp.get(sortedStr).add(str);
+          int[] arr=new int[26];
+          for(int i=0;i<str.length();i++){
+            arr[str.charAt(i)-'a']++;
+          }
+          
+           String keyStr=Arrays.toString(arr);
+          mp.putIfAbsent(keyStr,new ArrayList<>());
+         mp.get(keyStr).add(str);
         }
       return new ArrayList<>(mp.values());
     }
