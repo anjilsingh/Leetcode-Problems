bc5f81ca0021c22248f22237983e854562a2ca23
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-18 13:58:23 +0530

    Time: 7 ms (37.90%) | Memory: 43 MB (79.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //anagram means same character 
-        //so we have two options one to 
-        //soet the strings or can use a hashtable 
-        // or hashmap
         if(s.length()!=t.length()) return false;
-       HashMap<Character,Integer>ms=new HashMap<>();
-       HashMap<Character,Integer>mt=new HashMap<>();
-       
-      for(int i=0;i<s.length();i++){
-           ms.put(s.charAt(i),ms.getOrDefault(s.charAt(i),0)+1);
-           mt.put(t.charAt(i),mt.getOrDefault(t.charAt(i),0)+1);
-      }
+        int[] arr=new int[26];
+        for(int i=0;i<s.length();i++){
+        arr[s.charAt(i)-'a']++;
+        arr[t.charAt(i)-'a']--;
+        }
         
-     return ms.equals(mt);
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0) return false;
+        }
+        return true;
     }
 }
