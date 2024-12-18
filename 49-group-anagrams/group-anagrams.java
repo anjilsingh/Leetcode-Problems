89a8082c750aebd923959951f9968cc897c4f4ae
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-18 14:54:45 +0530

    Time: 9 ms (33.45%) | Memory: 48 MB (34.07%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,25 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        //we have to group anagrams 
-        //what is anagram 
-        //either sort or use a hashtable we kno it very well:)
-        
-        //can use a hashmap and put the values based on that 
-        
+        //first crate the res list of list of string
+   
         HashMap<String,List<String>>mp=new HashMap<>();
         for(String str:strs){
-            //or instead of sorting can use a hashtable 
-            int count[]=new int[26];
-             for(char c:str.toCharArray()){
-                count[c-'a']++;
-             }
-            String key=Arrays.toString(count);
-            mp.putIfAbsent(key,new ArrayList<>());
-            mp.get(key).add(str);
-        
+            char strchar[]=str.toCharArray();
+            Arrays.sort(strchar);
+            String sortedStr=new String(strchar);
+            mp.putIfAbsent(sortedStr,new ArrayList<>());
+            mp.get(sortedStr).add(str);
         }
-        
-        return new ArrayList<>(mp.values());
-        
+      return new ArrayList<>(mp.values());
     }
 }
