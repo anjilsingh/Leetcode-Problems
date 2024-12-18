0e6220c8bcd4ac49d51e2ac761d253fee043b15f
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-18 18:23:45 +0530

    Time: 44 ms (37.40%) | Memory: 45.1 MB (28.63%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,17 +1,10 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        //using hashmap in two
-        //pass
-        
-        
-        HashMap<Integer,Integer>mp=new HashMap<>();
+        //can use two pointer but that is easy if we have to directly do for number
         for(int i=0;i<nums.length;i++){
-           mp.put(nums[i],i);
-        }
-        
-        for(int i=0;i<nums.length;i++){
-          int req=target-nums[i];
-          if(mp.containsKey(req) && mp.get(req)!=i) return new int[]{i,mp.get(req)};
+          for(int j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]==target) return new int[]{i,j};
+          }
         }
         return new int[]{-1,-1};
     }
