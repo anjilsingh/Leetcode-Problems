007cc22e9477bcac7717f9b9b0b5c16fcdc36132
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-18 21:26:07 +0530

    Time: 1250 ms (5.06%) | Memory: 45.3 MB (7.26%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,24 +1,27 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        
+        //permutaion means same character
+        //so either to sort the string 
+        //or use a hashtable
+        //lets see the bruteforce approach
+        //generate all 
         
         if(s2.length()<s1.length()) return false;
-        if(s1.equals(s2)) return true;
-        
-        char[] s1ch=s1.toCharArray();
-        Arrays.sort(s1ch);
-        String ss1=new String(s1ch);
-        
+        char[] s1char=s1.toCharArray();
+        Arrays.sort(s1char);
+        String sortedS1=new String(s1char);
+            
         for(int i=0;i<s2.length();i++){
           for(int j=i;j<s2.length();j++){
-           if(j-i+1==s1.length()){
-               char[] wch=s2.substring(i,j+1).toCharArray();
-               Arrays.sort(wch);
-               String ss2=new String(wch);
-               if(ss2.equals(ss1)) return true;
-           }
+            if(j-i+1==s1.length()){
+                char[] win=s2.substring(i,j+1).toCharArray();
+                Arrays.sort(win);
+                String winSorted=new String(win);
+                if(sortedS1.equals(winSorted)) return true;
+            }
           }
         }
+        
         return false;
     }
 }
