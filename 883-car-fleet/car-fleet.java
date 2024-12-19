63a87ed0b35bd2f3280c86596a1f20bb1caff9d0
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-19 21:15:30 +0530

    Time: 76 ms (66.92%) | Memory: 59.3 MB (22.26%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,27 +1,21 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Stack;
-
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        ArrayList<int[]> arr = new ArrayList<>();
-        for (int i = 0; i < position.length; i++) {
-            arr.add(new int[]{position[i], speed[i]});
+        ArrayList<int[]>arr=new ArrayList<>();
+        for(int i=0;i<position.length;i++){
+           arr.add(new int[]{position[i],speed[i]});
         }
-
-        // Sort based on position in descending order
-        arr.sort((a, b) -> b[0] - a[0]);
-
-        Stack<Double> st = new Stack<>();
-        for (int[] p : arr) {
-            // Calculate time to reach the target
-            double time = (double) (target - p[0]) / p[1];
-            if (!st.isEmpty() && time <= st.peek()) {
-                // Merge fleets if necessary
-                continue;
-            }
-            st.push(time);
+        arr.sort((a,b)->b[0]-a[0]);
+        
+        
+        Stack<Double>st=new Stack<>();
+        for(int[] p:arr){
+           double dis=(double) (target-p[0])/p[1];
+           if(!st.isEmpty() && dis<=st.peek()){
+             //means they will collide and dont need to put further 
+               continue;
+           }
+            st.push(dis);
         }
-        return st.size();
+      return st.size();  
     }
 }
