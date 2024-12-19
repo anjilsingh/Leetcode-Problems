1c1246f2cbed2cdebfaee4cd60f2f9cf25909b23
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-19 08:30:00 +0530

    Time: 79 ms (14.84%) | Memory: 59.4 MB (45.01%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,54 +1,17 @@
 class Solution {
-     static class Pair {
-        int value;
-        int index;
-
-        public Pair(int value, int index)
-        {
-            this.value = value;
-            this.index = index;
-        }
-    }
     public int[] maxSlidingWindow(int[] nums, int k) {
-         if(nums.length<k){
-             
-             return nums;
-
-         }
-   
-        List<Integer>ans=new ArrayList<>();
-       PriorityQueue<Pair> heap = new PriorityQueue<>(
-            (a, b) -> b.value - a.value);
-
-        // Initialize the heap with the first k elements
-        for (int i = 0; i < k; i++) {
-            heap.offer(new Pair(nums[i], i));
-        }
-
-        // The maximum element in the first window
-        ans.add(heap.peek().value);
-
-        // Process the remaining elements
-        for (int i = k; i < nums.length; i++) {
-            heap.offer(new Pair(nums[i], i));
-
-            // Remove elements that are outside the current
-            // window
-            while (heap.peek().index <= i - k) {
-                heap.poll();
-            }
-
-            // The maximum element in the current window
-            ans.add(heap.peek().value);
-        }
-
-       
+        int n=nums.length;
+        int[] output=new int[n-k+1];
+        PriorityQueue<int[]>maxHeap=new PriorityQueue<>((a,b)->b[0]-a[0]);
         
-        int[] res = new int[ans.size()];
-        for (int i = 0; i < ans.size(); i++) {
-            res[i] = ans.get(i);
+        int idx=0;
+        for(int i=0;i<nums.length;i++){
+             maxHeap.offer(new int[]{nums[i],i});
+            if(i>=k-1){
+               while(maxHeap.peek()[1]<=i-k) maxHeap.poll();
+                output[idx++]=maxHeap.peek()[0];
+            }
         }
-
-        return res;
+        return output;
     }
 }
