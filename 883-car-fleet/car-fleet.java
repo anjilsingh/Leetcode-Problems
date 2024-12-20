303cbc7999006f66a452cdfd7264a72b9987d827
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-20 17:53:20 +0530

    Time: 75 ms (69.79%) | Memory: 60 MB (17.80%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -2,20 +2,22 @@ class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
         ArrayList<int[]>arr=new ArrayList<>();
         for(int i=0;i<position.length;i++){
-           arr.add(new int[]{position[i],speed[i]});
+            arr.add(new int[]{position[i],speed[i]});
         }
         arr.sort((a,b)->b[0]-a[0]);
         
         
         Stack<Double>st=new Stack<>();
         for(int[] p:arr){
-           double dis=(double) (target-p[0])/p[1];
-           if(!st.isEmpty() && dis<=st.peek()){
-             //means they will collide and dont need to put further 
-               continue;
-           }
-            st.push(dis);
+          double dis=(double)(target-p[0])/p[1];
+          
+          if(!st.isEmpty() && dis<=st.peek()){
+             continue;
+            //they will gonna collide  
+              
+          }
+          st.push(dis);
         }
-      return st.size();  
+        return st.size();
     }
 }
