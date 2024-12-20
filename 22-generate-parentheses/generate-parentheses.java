58460deb11d902c5ab820d38efa86c2f8ac1d274
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-20 16:22:02 +0530

    Time: 1 ms (82.80%) | Memory: 42.7 MB (87.52%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,23 +1,24 @@
 class Solution {
-    public void backtrack(int openN,int closedN,int n,List<String>res,StringBuilder sb)     {
-        if(openN==n && closedN==n){
-           res.add(sb.toString());
-           return;
+    
+    public void backtrack(int openN,int closedN,int n, List<String>res,StringBuilder sb)    {
+
+    if(openN==n && closedN==n){
+       res.add(sb.toString());
+       return;
+    }
+   //explore left side
+        if(openN<n){
+            sb.append('(');
+            backtrack(openN+1,closedN,n,res,sb);
+            sb.deleteCharAt(sb.length()-1);
         }
         
-        if(openN<n){
-            
-           sb.append('(');
-           backtrack(openN+1,closedN,n,res,sb);
-           sb.deleteCharAt(sb.length()-1);
-        }
         if(closedN<openN){
-            
            sb.append(')');
-           backtrack(openN,closedN+1,n,res,sb);
-           sb.deleteCharAt(sb.length()-1);
+            backtrack(openN,closedN+1,n,res,sb);
+             sb.deleteCharAt(sb.length()-1);
         }
-
+    
     }
     public List<String> generateParenthesis(int n) {
         List<String>res=new ArrayList<>();
