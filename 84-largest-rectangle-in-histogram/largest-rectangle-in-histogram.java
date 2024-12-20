6d5ed3b152037deeea5e395223fc37c101e5f5c7
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-20 21:00:15 +0530

    Time: 49 ms (79.47%) | Memory: 56.4 MB (95.02%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,27 +1,30 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
+        //second way is to use 
+        //stack
+        //for any index we push it till we can and when we remove it 
+        
         int n=heights.length;
-        int area=0;
         Stack<int[]>st=new Stack<>();
-        
+        int marea=0;
         for(int i=0;i<n;i++){
-         int start=i;
-         while(!st.isEmpty() && st.peek()[1]>heights[i]){
-            int[] ele=st.pop();
-            int index=ele[0];
-            int val=ele[1];
-            area=Math.max(area,(i-index)*val);
-            start=index;
-         }
-        st.push(new int[]{start,heights[i]});
+            int start=i;
+            while(!st.isEmpty() && st.peek()[1]>heights[i]){
+                int ele[]=st.pop();
+                int index=ele[0];
+                int val=ele[1];
+                marea=Math.max(marea,(i-index)*val);
+                start=index;
+            }
+            st.push(new int[]{start,heights[i]});
         }
-        
-        
-        for(int[] p:st){
-             int ind=p[0];
-             int value=p[1];
-            area=Math.max(area,(n-ind)*value);
+       
+        while(!st.isEmpty()){
+           int rem[]=st.pop();
+           int ind=rem[0];
+           int value=rem[1];
+            marea=Math.max(marea,(n-ind)*value);
         }
-        return area;
+        return marea;
     }
 }
