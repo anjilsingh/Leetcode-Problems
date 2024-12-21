1b6ca2183e4d0758d7296c71d11487627a777d76
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-21 08:28:53 +0530

    Time: 27 ms (24.03%) | Memory: 45.4 MB (16.57%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,53 +1,29 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-      // so we know that the h will be always greater then the 
-    // length of piles
-        
-        //we can iterate from 1 to max
+        //now instead of this we can use binary search 
         
         int max=piles[0];
-        for(int i=1;i<piles.length;i++){
-            
-            if(piles[i]>max){
-                max=piles[i];
-
-             }
+        for(int i=0;i<piles.length;i++){
+            if(piles[i]>max) max=piles[i];
         }
-        int res=max;
+        int ans=max;
         int left=0;
         int right=max;
         
         while(left<=right){
-            int mid=(left+right)/2;
-            
-            int sum=0;
-            for(int j=0;j<piles.length;j++){
-                sum+=Math.ceil((double)piles[j]/mid);
-
+           int mid=(left+right)/2;
+           int total=0;
+            for(int i=0;i<piles.length;i++){
+              total+=Math.ceil((double)piles[i]/mid);
             }
-            if(sum<=h){
-                res=mid;
+            if(total>h){
+              left=mid+1;
+            }
+            else{
+                ans=mid;
                 right=mid-1;
-
             }
-            else if(sum>h){
-               left=mid+1;
-            }
-
         }
-        return res;
-//         for(int i=1;i<max;i++){
-//               int sum=0;
-//             for(int j=0;j<piles.length;j++){
-//               sum+=Math.ceil((double)piles[j]/i);
-
-//             }
-//             if(sum<=h){
-//                res=i;
-//                return res;
-//             }
-
-//         }
-//         return res;
+        return ans;
     }
 }
