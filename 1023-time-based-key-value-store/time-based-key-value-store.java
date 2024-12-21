93cc59bf703f597b53670638aa44ca0f996d55c2
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-21 11:31:31 +0530

    Time: 137 ms (73.15%) | Memory: 118.2 MB (86.98%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,49 +1,47 @@
-
-
 class TimeMap {
-    private Map<String, List<Pair>> map;
-
+    private static class Pair{
+      int ts;
+     String val;
+     Pair(int ts,String val){
+        this.ts=ts;
+        this.val=val;
+     }
+    }
+     //first create a hashmap
+    HashMap<String,List<Pair>>mp;
+    
     public TimeMap() {
-        map = new HashMap<>();
+        mp=new HashMap<>();
     }
-
+    
     public void set(String key, String value, int timestamp) {
-        map.putIfAbsent(key, new ArrayList<>());
-        map.get(key).add(new Pair(timestamp, value));
+        mp.putIfAbsent(key,new ArrayList<>());
+        mp.get(key).add(new Pair(timestamp,value));
     }
-
+    
     public String get(String key, int timestamp) {
-        if (!map.containsKey(key)) return "";
-
-        List<Pair> list = map.get(key);
-        int index = binarySearch(list, timestamp);
-        return index == -1 ? "" : list.get(index).value;
-    }
-
-    private int binarySearch(List<Pair> list, int timestamp) {
-        int left = 0, right = list.size() - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (list.get(mid).timestamp <= timestamp) {
-                left = mid + 1; // Search right
-            } else {
-                right = mid - 1; // Search left
+        if(!mp.containsKey(key)) return "";
+        List<Pair>list=mp.get(key);
+       String res="";
+       int left=0;
+       int right=list.size()-1;
+        while(left<=right){
+          int mid=left+(right-left)/2;
+          if(list.get(mid).ts<=timestamp){
+              res=list.get(mid).val;
+              left=mid+1;
+          }
+            else{
+                 right=mid-1;
             }
         }
-
-        return right; // Largest index with timestamp <= input
-    }
-
-    private static class Pair {
-        int timestamp;
-        String value;
-
-        Pair(int timestamp, String value) {
-            this.timestamp = timestamp;
-            this.value = value;
-        }
+        return res;
     }
 }
 
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
