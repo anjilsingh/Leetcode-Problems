5b4980175491b1c53b6addc7440795509fc7a764
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-21 13:51:48 +0530

    Time: 2 ms (58.94%) | Memory: 45.9 MB (67.62%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,21 +1,57 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        //better way to not use sorting here 
+        //just sort the array directly 
+        //like we did in  merge sort
+        //now  better way is to 
+        //we have to just find ind1 and ind2 
+        //these ind are nothing but n/2-1 and n/2
         
-        int arr[]=new int[nums1.length+nums2.length];
-         int ind=0;
-        for(int num:nums1){
-         arr[ind]=num;
-            ind++;
-        }
-        for(int num:nums2){
-          arr[ind]=num;
-            ind++;
+        int len1=nums1.length;
+        int len2=nums2.length;
+        int total=len1+len2;
+        //ind1 will be total/2-1
+        //and ind2 will be total/2
+        int ind1=-1;
+        int ind2=-1;
+        int i=0;
+        int j=0;
+        int index=-1;
+        int curr=0;
+        while(i<len1 && j<len2){
+           
+            
+             if(nums1[i]<=nums2[j]){
+               curr=nums1[i];
+               i++;
+               
+             }
+            else{
+                curr=nums2[j];
+             j++;
+            }
+            index++;
+           if(index==total/2-1)  ind1=curr;
+           if(index==total/2)    ind2=curr;
         }
         
-        Arrays.sort(arr);
-        int n=arr.length;
-        //even
-        if(n%2==0) return (double)(arr[n/2-1]+arr[n/2])/2;
-        else return (double)arr[n/2];
+        while(i<len1){
+             index++;
+             curr=nums1[i];
+             i++;
+           if(index==total/2-1)  ind1=curr;
+           if(index==total/2)    ind2=curr;
+        }
+        
+         while(j<len2){
+             index++;
+             curr=nums2[j];
+             j++;
+            if(index==total/2-1)  ind1=curr;
+           if(index==total/2)    ind2=curr;
+             }
+        
+        if(total%2==0)  return (double)(ind1+ind2)/2;
+        else return (double) ind2;
     }
 }
