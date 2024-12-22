668ff7e97b03768e3cb966df93fe09b85cafab03
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-22 20:59:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (6.93%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,34 +1,36 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row=matrix.length;
+        
+        int row=matrix.length-1;
         int col=matrix[0].length-1;
         
-        //first search the target row using bs
         int top=0;
-        int bot=row-1;
+        int bot=row;
+        while(top<=bot){
+            int mid=(top+bot)/2;
+            if(target<matrix[mid][0]) {
+             bot=mid-1;
+            }
+            else if(target>matrix[mid][col]){
+              top=mid+1;
+            }
+            else{
+              break;
+            }
+        }
+     
+        if(bot<top){
+            return false;
+        }
+        int ro=(top+bot)/2;
         
-       while(top<=bot) {
-         int r=(top+bot)/2;
-         if(target>matrix[r][col]){
-             top=r+1;
-         }
-         else if(target<matrix[r][0]){
-             bot=r-1;
-         }
-           else {
-                break;
-           }
-       }
-        if(top>bot) return false;
-        int r=(top+bot)/2;
-        
-        int left=0;
-        int right=col;
-        while(left<=right){
-          int mid=(left+right)/2;
-          if(matrix[r][mid]==target) return true;
-          else if(matrix[r][mid]<target) left=mid+1;
-          else  right=mid-1;
+        int l=0;
+        int r=col;
+        while(l<=r){
+           int mid=(l+r)/2;
+            if(matrix[ro][mid]==target) return true;
+            else if(matrix[ro][mid]<target) l=mid+1;
+            else r=mid-1;
         }
         return false;
     }
