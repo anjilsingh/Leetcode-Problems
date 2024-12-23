e76b9e98e9c2ba867259c3b7142ac442c8b6f023
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-23 21:49:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (75.43%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,16 +11,15 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-    if (head == null || head.next == null) return false; // No cycle
-    return hasCycle(head, head.next); // Start the recursion
-}
-
-public boolean hasCycle(ListNode slow, ListNode fast) {
-    // Base cases
-    if (fast == null || fast.next == null) return false; // No cycle
-    if (slow == fast) return true; // Cycle detected
-
-    // Recursive call with updated pointers
-    return hasCycle(slow.next, fast.next.next);
-}
+        //lets do it by using slow and fast pointer 
+        if(head==null ||head.next==null) return false;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow) return true;
+        }
+        return false;
+    }
 }
