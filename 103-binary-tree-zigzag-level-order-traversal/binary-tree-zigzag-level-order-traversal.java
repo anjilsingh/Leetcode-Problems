8282c889abaabe4168dd07988102f72ab9ad4425
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-25 00:28:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (8.82%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -15,37 +15,33 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-        if (root == null) return ans;
-
-        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>>ans=new ArrayList<>();
+        if(root==null) return ans;
+        
+        Queue<TreeNode>q=new LinkedList<>();
         q.add(root);
-        boolean flag = true; // true means left-to-right, false means right-to-left
-
-        while (!q.isEmpty()) {
-            List<Integer> temp = new ArrayList<>();
-            int size = q.size();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                
-                // Simply add values to `temp`
-                temp.add(node.val);
-                
-                // Add children to queue
-                if (node.left != null) q.add(node.left);
-                if (node.right != null) q.add(node.right);
-            }
-
-            // Reverse `temp` if flag is false for right-to-left order
-            if (!flag) {
-                Collections.reverse(temp);
-            }
+        boolean ltor=true;
+        while(!q.isEmpty()){
+          List<Integer>res=new ArrayList<>();
+          int size=q.size();
+          for(int i=0;i<size;i++){
+              TreeNode node=q.poll();
+              res.add(node.val);
+              if(node.left!=null){
+                q.add(node.left);
+              }
+              if(node.right!=null){
+                    q.add(node.right);
+              }
+          }
+            if(ltor) ans.add(res);
             
-            ans.add(temp);
-            flag = !flag; // Toggle flag for next level
+            else {
+                Collections.reverse(res);
+                ans.add(res);
+                }
+            ltor=!ltor;
         }
-
         return ans;
     }
 }
