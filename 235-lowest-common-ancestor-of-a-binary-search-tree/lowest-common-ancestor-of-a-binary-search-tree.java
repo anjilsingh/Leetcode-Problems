2024ff4ab9f17f8ef4da1b7947d2f228995fe02e
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-25 14:37:39 +0530

    Time: 5 ms (100.00%) | Memory: 45.2 MB (27.63%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -7,38 +7,18 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        int small = Math.min(p.val, q.val);
-        int large = Math.max(p.val, q.val);
-        while (root != null) {
-            if (root.val > large) // p, q belong to the left subtree
-                root = root.left;
-            else if (root.val < small) // p, q belong to the right subtree
-                root = root.right;
-            else // Now, small <= root.val <= large -> This root is the LCA between p and q
-                return root;
+        
+        if(root.val==p.val|| root.val==q.val) return root;
+        if((p.val<root.val && q.val> root.val)||(p.val>root.val && q.val <root.val)) return root;
+        
+        if(p.val<root.val && q.val<root.val){
+            return lowestCommonAncestor(root.left,p,q);
         }
-        return null;
+        else{
+ return lowestCommonAncestor(root.right,p,q);
+        }
     }
 }
-// class Solution {
-//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-//         int max=Math.max(p.val,q.val);
-//         int min=Math.min(p.val,q.val);
-//         while(root!=null)
-//         {  
-//             if(root.val>max){
-//               root=root.left;
-//             }
-//             else if(root.val<max){
-//              root=root.right;
-//             }
-//             else{
-//              return root;
-//             }
-
-//         }
-//         return null;
-//     }
-// }
