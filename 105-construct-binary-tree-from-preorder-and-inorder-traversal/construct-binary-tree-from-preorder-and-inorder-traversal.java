93fa694cbb1036e0b43d754b04c87b40d929e5aa
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-29 01:34:33 +0530

    Time: 9 ms (6.06%) | Memory: 83.7 MB (7.37%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,26 +15,25 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder.length == 0 || inorder.length == 0) {
-            return null;
-        }
+        if(preorder.length==0||inorder.length==0) return null;
 
-        TreeNode root = new TreeNode(preorder[0]);
-        int mid = -1;
-        for (int i = 0; i < inorder.length; i++) {
-            if (inorder[i] == preorder[0]) {
-                mid = i;
-                break;
+        int ele=preorder[0];
+        int mid=-1;
+        for(int i=0;i<inorder.length;i++){
+            if(inorder[i]==ele){
+              mid=i;
+              break;
+
             }
         }
+        TreeNode root=new TreeNode(ele);
+        int[] leftPre=Arrays.copyOfRange(preorder,1,mid+1);
+        int[] leftIn=Arrays.copyOfRange(inorder,0,mid);
+        root.left=buildTree(leftPre,leftIn);
 
-        int[] leftPreorder = Arrays.copyOfRange(preorder, 1, mid + 1);
-        int[] leftInorder = Arrays.copyOfRange(inorder, 0, mid);
-        root.left = buildTree(leftPreorder, leftInorder);
-
-        int[] rightPreorder = Arrays.copyOfRange(preorder, mid + 1, preorder.length);
-        int[] rightInorder = Arrays.copyOfRange(inorder, mid + 1, inorder.length);
-        root.right = buildTree(rightPreorder, rightInorder);
+        int[] rPre=Arrays.copyOfRange(preorder,mid+1,preorder.length);
+        int[] rIn=Arrays.copyOfRange(inorder,mid+1,inorder.length);
+        root.right=buildTree(rPre,rIn);
 
         return root;
     }
