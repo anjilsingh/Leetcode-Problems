f12cf67178f577639980b49641ce678455276173
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-30 00:07:25 +0530

    Time: 11 ms (72.11%) | Memory: 58 MB (54.92%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -3,15 +3,13 @@ class Solution {
         //4 approach
         //2 for loop,sort,hashmap,set
 
-         HashMap<Integer,Integer>mp=new HashMap<>();
-
-         for(int i=0;i<nums.length;i++){
-            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
-         }
-
-          for(int i=0;i<nums.length;i++){
-            if(mp.get(nums[i])>1) return true;
-          }
-          return false;
+       HashSet<Integer>set=new HashSet<>();
+       for(int num:nums){
+        if(set.contains(num)){
+                return true;
+        }
+        set.add(num);
+       }
+       return false;
     }
 }
