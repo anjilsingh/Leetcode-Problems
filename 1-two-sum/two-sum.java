183fec3355763e9c9be23423e21e08d035af1845
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2024-12-30 00:18:47 +0530

    Time: 44 ms (37.63%) | Memory: 44.8 MB (76.44%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,10 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        //can use two pointer but that is easy if we have to directly do for number
         for(int i=0;i<nums.length;i++){
-          for(int j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target) return new int[]{i,j};
-          }
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
         }
         return new int[]{-1,-1};
     }
