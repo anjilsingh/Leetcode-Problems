b022f4738c0d11db40bda44981db75ca44fbef54
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-03 21:33:21 +0530

    Time: 2 ms (77.47%) | Memory: 44.6 MB (44.12%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,60 +1,51 @@
 class Solution {
-       public boolean isValid( char[][] board,int r ,int c) {
-        HashSet<Character> hs = new HashSet<>();
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[r + i][c + j] == '.') continue;
-                else {
-                    if (hs.contains(board[r + i][c + j])) return false;
-                    else {
-                        hs.add(board[r + i][c + j]);
-                    }
-                }
+    public boolean isValid(int row,int col,char[][]board){
+      HashSet<Character>seen=new HashSet<>();
+      for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(board[i+row][j+col]=='.') continue;
+            else {
+                if(seen.contains(board[row+i][col+j])) return false;
+                seen.add(board[row+i][col+j]);
             }
         }
-        return true;
+      }
+      return true;
     }
-
     public boolean isValidSudoku(char[][] board) {
         
-        //so first we need to check for rows
-        //then check for cols
-        //then all 3*3 matrix
-        
-        for(int i=0;i<9;i++){
-          HashSet<Character>seen=new HashSet<>();
-          for(int j=0;j<9;j++){
-              if(board[i][j]=='.') continue;
-              else{ 
-                 if(seen.contains(board[i][j])) return false;
-                 else{
-                     seen.add(board[i][j]);
-                 }
-              }
-          }
-        }
-        
-           for(int i=0;i<9;i++){
-          HashSet<Character>seen=new HashSet<>();
-          for(int j=0;j<9;j++){
-              if(board[j][i]=='.') continue;
-              else{ 
-                 if(seen.contains(board[j][i])) return false;
-                 else{
-                     seen.add(board[j][i]);
-                 }
-              }
-          }
-        }
-        
-        //now check for all 3*3 matrix
-        
-        for(int i=0;i<9;i=i+3){
-            for(int j=0;j<9;j=j+3){
-             if(!isValid(board,i,j)) return false;
+     //check for row
+     for(int i=0;i<9;i++){
+        HashSet<Character>seen=new HashSet<>();
+        for(int j=0;j<9;j++){
+            if(board[i][j]=='.') continue;
+            else{
+                if(seen.contains(board[i][j])) return false;
+                seen.add(board[i][j]);
             }
         }
-        return true;
-        
+     }
+     for(int i=0;i<9;i++){
+        HashSet<Character>seen=new HashSet<>();
+        for(int j=0;j<9;j++){
+            if(board[j][i]=='.') continue;
+            else{
+                if(seen.contains(board[j][i])) return false;
+                seen.add(board[j][i]);
+            }
+        }
+     }
+
+     for(int i=0;i<9;i=i+3){
+        for(int j=0;j<9;j=j+3){
+           // if(!isValid(i,j,board)) return false;
+
+          // return isValid(i,j,board):true?false;
+          if(isValid(i,j,board)==false){
+            return  false;
+          }
+        }
+     }
+     return true;
     }
 }
