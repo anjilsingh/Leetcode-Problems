3297138317689e20e0586d761a007618ad7c509e
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-03 20:29:25 +0530

    Time: 3 ms (90.40%) | Memory: 43 MB (86.82%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,13 +3,13 @@ class Solution {
         int[] arr=new int[26];
         for(int i=0;i<s.length();i++){
             arr[s.charAt(i)-'a']++;
+      
         }
         for(int i=0;i<t.length();i++){
             arr[t.charAt(i)-'a']--;
         }
-
         for(int i=0;i<26;i++){
-            if(arr[i]!=0) return false;
+            if(arr[i]!=0)  return false;
         }
         return true;
     }
