815ed540ac1b12fbfa6b4941323d7dfac5fcc703
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-03 22:22:03 +0530

    Time: 5 ms (74.44%) | Memory: 58 MB (50.43%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,23 +1,19 @@
 class Solution {
     public int maxArea(int[] height) {
-       //now lets see how two pointer works here 
-        
-        
-        //so we will calculate area for a particular pos and then move pointer furtheerr 
-        //from less side to more side
-        int maxArea=0;
+        int mArea=0;
         int l=0;
         int r=height.length-1;
         while(l<r){
-           int area=Math.min(height[l],height[r])*(r-l);
-           maxArea=Math.max(area,maxArea);
-            if(height[l]<height[r]){
-              l++;
+            int area=Math.min(height[l],height[r])*(r-l);
+            mArea=Math.max(mArea,area);
+
+            if(height[l]<=height[r]){
+                l++;
             }
-            else{
+            else if(height[r]<height[l]){
                 r--;
             }
         }
-        return maxArea;
+        return mArea;
     }
 }
