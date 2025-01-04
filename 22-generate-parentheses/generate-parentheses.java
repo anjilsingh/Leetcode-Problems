b483adc56aed4a90fae45e7c278a918df1c565e1
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-04 22:51:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (84.50%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,29 +1,24 @@
 class Solution {
-    
-    public void backtrack(int openN,int closedN,int n, List<String>res,StringBuilder sb)    {
-
-    if(openN==n && closedN==n){
-       res.add(sb.toString());
-       return;
-    }
-   //explore left side
+    public static void gP(List<String>res,StringBuilder sb,int openN,int closedN,int n){
+        if(openN==n && closedN==n){
+            res.add(sb.toString());
+            return;
+        }
         if(openN<n){
             sb.append('(');
-            backtrack(openN+1,closedN,n,res,sb);
-            sb.deleteCharAt(sb.length()-1);
+            gP(res,sb,openN+1,closedN,n);
+           sb.deleteCharAt(sb.length() - 1);
         }
-        
         if(closedN<openN){
-           sb.append(')');
-            backtrack(openN,closedN+1,n,res,sb);
-             sb.deleteCharAt(sb.length()-1);
+            sb.append(')');
+            gP(res,sb,openN,closedN+1,n);
+           sb.deleteCharAt(sb.length() - 1);
         }
-    
     }
     public List<String> generateParenthesis(int n) {
         List<String>res=new ArrayList<>();
         StringBuilder sb=new StringBuilder();
-        backtrack(0,0,n,res,sb);
+        gP(res,sb,0,0,n);
         return res;
     }
 }
