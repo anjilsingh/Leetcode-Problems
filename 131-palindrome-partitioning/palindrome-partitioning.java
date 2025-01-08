269e94558e782e0d352c2aaf5679772761a83f0c
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-08 10:46:04 +0530

    Time: 9 ms (30.39%) | Memory: 56.5 MB (82.47%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,29 +1,34 @@
 class Solution {
-    public List<List<String>> partition(String s) {
-        List<List<String>>res=new ArrayList<>();
-        List<String>path=new ArrayList<>();
-        func(0,s,res,path);
-        return res;
-        
-    }
-    static void func(int index,String s,List<List<String>>res,List<String>path){
-       if(index==s.length()){
-          res.add(new ArrayList<>(path));
-           return;
-       }
-        for(int i=index;i<s.length();++i){
-          if(isPalindrome(s,index,i)){
-               path.add(s.substring(index,i+1));
-              func(i+1,s,res,path);
-              path.remove(path.size() - 1);
-          }
-        }
-    }
-        static boolean isPalindrome(String s, int start, int end) {
-        while (start <= end) {
-            if (s.charAt(start++) != s.charAt(end--))
+    public boolean isPalin(String s){
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
                 return false;
+            }
+            left++;
+            right--;
         }
         return true;
+    }
+    public void solve(String s,int index,int n,List<List<String>>res,List<String>curr){
+        //base case 
+        if(index>=n){
+            res.add(new ArrayList<>(curr));
+        }
+        //rec solve
+        for(int i=index;i<n;i++){
+            if(isPalin(s.substring(index,i+1))){
+                curr.add(s.substring(index,i+1));
+                solve(s,i+1,n,res,curr);
+                curr.remove(curr.size()-1);
+            }
+        }
+    }
+    public List<List<String>> partition(String s) {
+        List<List<String>>res=new ArrayList<>();
+        int n=s.length();
+        solve(s,0,n,res,new ArrayList<>());
+        return res;
     }
 }
