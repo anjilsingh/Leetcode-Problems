44b6d29c6f3d6fd7a6f1a49fef4358e150534f52
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-10 00:15:35 +0530

    Time: 1 ms (93.65%) | Memory: 44.7 MB (26.63%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,19 +1,28 @@
 class Solution {
-    public void solve(List<List<Integer>>res,int[] nums,List<Integer>curr){
-        if(curr.size()==nums.length){
+    public void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public void solve(int start,List<List<Integer>>res,int[] nums){
+        if(start==nums.length-1){
+            List<Integer>curr=new ArrayList<>();
+            for(int num:nums){
+                curr.add(num);
+            }
             res.add(new ArrayList<>(curr));
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(curr.contains(nums[i])) continue;
-            curr.add(nums[i]);
-            solve(res,nums,curr);
-            curr.remove(curr.size()-1);
+        for(int i=start;i<nums.length;i++){
+           swap(nums,start,i);
+           solve(start+1,res,nums);
+           swap(nums,start,i);
+
         }
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>>res=new ArrayList<>();
-        solve(res,nums,new ArrayList<>());
+        solve(0,res,nums);
         return res;
 
     }
