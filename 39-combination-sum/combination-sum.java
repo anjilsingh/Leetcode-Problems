2e94f896425958084dfc7caa593d2a1edc886654
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-09 23:58:15 +0530

    Time: 2 ms (83.21%) | Memory: 44.7 MB (56.44%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,20 +1,23 @@
 class Solution {
-    public static void solve(int[] candidates,int target,int sum,List<List<Integer>>ans,List<Integer>curr,int index){
+    public static void solve(int[] candidates,int target,int sum,List<List<Integer>>ans,List<Integer>curr,int index,int n){
         if(target==sum){
             ans.add(new ArrayList<>(curr));
             return;
         }
-        if(sum>target||index>=candidates.length) return;
-        //one time include the curr ele 
-        curr.add(candidates[index]);
-        solve(candidates,target,sum+candidates[index],ans,curr,index);
-        //but when we dont include it we need to move one index further
-        curr.remove(curr.size()-1);
-        solve(candidates,target,sum,ans,curr,index+1);
+        if(sum>target) {return;}
+        for(int i=index;i<n;i++){
+           
+            curr.add(candidates[i]);
+            solve(candidates,target,sum+candidates[i],ans,curr,i,n);
+    
+            curr.remove(curr.size()-1);
+        }
+        
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>>ans=new ArrayList<>();
-        solve(candidates,target,0,ans,new ArrayList<>(),0);
+        int n=candidates.length;
+        solve(candidates,target,0,ans,new ArrayList<>(),0,n);
         return ans;
     }
 }
