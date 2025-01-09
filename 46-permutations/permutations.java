488ac09af9a00d7268721c8227daf9b0809ea0d4
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-09 20:25:56 +0530

    Time: 2 ms (42.93%) | Memory: 44.4 MB (72.59%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,32 +1,21 @@
 class Solution {
-    public void swap(int[] nums,int index,int i){
-            int temp=nums[index];
-            nums[index]=nums[i];
-            nums[i]=temp;
+    public void solve(int n,List<List<Integer>>res,int[] nums,List<Integer>curr){
+        if(curr.size()==n){
+            res.add(new ArrayList<>(curr));
         }
-    public void solve(int[] nums,int index,int n,List<List<Integer>>res){
-        //base case
-        if(index==n-1){
-            List<Integer>cur=new ArrayList<>();
-            for(int num:nums){
-                cur.add(num);
-            }
-            res.add(new ArrayList<>(cur));
-            return;
+        for(int i=0;i<n;i++){
+           if(curr.contains(nums[i])) {
+                continue;
+           }
+           curr.add(nums[i]);
+           solve(n,res,nums,curr);
+           curr.remove(curr.size()-1);
         }
-        //swap and call for next
-        for(int i=index;i<n;i++){
-            swap(nums,index,i);
-            solve(nums,index+1,n,res);
-            swap(nums,index,i);
-        }
-
-        
     }
     public List<List<Integer>> permute(int[] nums) {
-      List<List<Integer>>res=new ArrayList<>();
-      int n=nums.length;
-      solve(nums,0,n,res);
-      return res;
+        List<List<Integer>>res=new ArrayList<>();
+        int n=nums.length;
+        solve(n,res,nums,new ArrayList<>());
+        return res;
     }
 }
