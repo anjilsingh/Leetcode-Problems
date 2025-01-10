2c62a968f9801f05a6c8470cf51d9299c2c64df0
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-10 20:41:59 +0530

    Time: 5 ms (19.62%) | Memory: 43.3 MB (45.61%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,23 +1,24 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List < List < Integer >> ans = new ArrayList < > ();
-        Arrays.sort(candidates);
-        findCombinations(0, candidates, target, ans, new ArrayList < > ());
-        return ans;
-    }
-    static void findCombinations(int ind, int[] arr, int target, List < List < Integer >> ans, List < Integer > ds) {
-        if (target == 0) {
-            ans.add(new ArrayList < > (ds));
+    public void solve(int[] candidates,List<List<Integer>>res,List<Integer>curr,int start,int sum,int target){
+        if(sum==target){
+            res.add(new ArrayList<>(curr));
             return;
         }
-
-        for (int i = ind; i < arr.length; i++) {
-            if (i > ind && arr[i] == arr[i - 1]) continue;
-            if (arr[i] > target) break;
-
-            ds.add(arr[i]);
-            findCombinations(i + 1, arr, target - arr[i], ans, ds);
-            ds.remove(ds.size() - 1);
+        if(sum>target){
+            return;
         }
+        for(int i=start;i<candidates.length;i++){
+            if(i>start && candidates[i]==candidates[i-1]) continue;
+            curr.add(candidates[i]);
+            solve(candidates,res,curr,i+1,sum+candidates[i],target);
+            curr.remove(curr.size()-1);
+        }
+
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>>res=new ArrayList<>();
+        solve(candidates,res,new ArrayList<>(),0,0,target);
+        return res;
     }
 }
