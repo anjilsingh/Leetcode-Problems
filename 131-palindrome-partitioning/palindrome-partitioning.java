1d849df9f87481f6655fbd732ab9a4d3330907e1
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-10 20:57:23 +0530

    Time: 9 ms (30.35%) | Memory: 56.2 MB (91.61%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,32 +1,29 @@
 class Solution {
     public boolean isPalin(String s){
-    int left=0;
-    int right=s.length()-1;
-    while(left<right){
-        if(s.charAt(left)!=s.charAt(right)){
-            return false;
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)) return false;
+            left++;
+            right--;
         }
-        left++;
-        right--;
+        return true;
     }
-    return true;
-    }
-    public void solve(String s,List<List<String>>res,List<String>curr,int start){
+    public void solve(int start,List<List<String>>res,List<String>curr,String s){
         if(start==s.length()){
             res.add(new ArrayList<>(curr));
-            return;
         }
         for(int i=start;i<s.length();i++){
             if(isPalin(s.substring(start,i+1))){
                 curr.add(s.substring(start,i+1));
-                solve(s,res,curr,i+1);
+                solve(i+1,res,curr,s);
                 curr.remove(curr.size()-1);
             }
         }
     }
     public List<List<String>> partition(String s) {
         List<List<String>>res=new ArrayList<>();
-        solve(s,res,new ArrayList<>(),0);
+        solve(0,res,new ArrayList<>(),s);
         return res;
     }
 }
