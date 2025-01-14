b2f4a1754522534e3a44a9f1ce8df02f2dfef1cf
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-14 22:07:55 +0530

    Time: 2 ms (97.49%) | Memory: 44.3 MB (62.40%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -14,17 +14,16 @@
  * }
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true; // Both trees are null, they are the same
-        if (p == null || q == null) return false; // One tree is null, the other is not
-        return (p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
-    }
+    public boolean isSameTree(TreeNode p,TreeNode q){
+        if(p==null && q==null) return true;
+        if(p==null ||q==null) return false;
 
+        return (p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right));
+    }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if (subRoot == null) return true; // An empty tree is a subtree of any tree
-        if (root == null) return false;  // A non-empty subtree cannot be in a null tree
-        if (isSameTree(root, subRoot)) return true; // Check if the current tree matches
-        // Recursively check the left and right subtrees
-        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        //base case if root become nulll
+        if(root==null) return  false;
+        if(isSameTree(root,subRoot)) return true;
+        return (isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot));
     }
 }
