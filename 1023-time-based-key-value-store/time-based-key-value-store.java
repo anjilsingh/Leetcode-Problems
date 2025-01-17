68e14202673aa78a70be02133c598e42ab34d3c0
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-17 10:20:53 +0530

    Time: 140 ms (65.41%) | Memory: 118.5 MB (81.77%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,45 +1,48 @@
-class TimeMap {
-    public static class Pair{
-      int ts;
-      String val;
-        Pair(int ts,String val){
-            this.ts=ts;
-            this.val=val;
-        }
+ public class Pair{
+    String val;
+    int ts;
+    public Pair(String val, int ts){
+        this.val=val;
+        this.ts=ts;
     }
-    
-    HashMap<String,List<Pair>>mp;
+ }
+class TimeMap {
+   HashMap<String,ArrayList<Pair>>mp;
     public TimeMap() {
         mp=new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
         mp.putIfAbsent(key,new ArrayList<>());
-        mp.get(key).add(new Pair(timestamp,value));
+        mp.get(key).add(new Pair(value,timestamp));
     }
     
     public String get(String key, int timestamp) {
-        if(!mp.containsKey(key)) return "";
-        String ans="";
+        if(!mp.containsKey(key)){
+            return "";
+        }
+        String res="";
         List<Pair>al=mp.get(key);
-        
-        int l=0;
-        int r=al.size()-1;
-        
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(al.get(mid).ts==timestamp) return al.get(mid).val;
-            else if(al.get(mid).ts<timestamp)
-            {ans= al.get(mid).val;
-              l=mid+1;
-             }
-            else {
-              r=mid-1;
+        int low=0;
+        int high=al.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            
+            int timeS=al.get(mid).ts;
+            if(timeS==timestamp){
+                res=al.get(mid).val;
+                return res;
+            }
+            else if(timeS<timestamp){
+                res=al.get(mid).val;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
-        
-        return ans;
-        
+        return res;
+
     }
 }
 
