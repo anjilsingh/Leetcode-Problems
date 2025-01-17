fa731b7a9aff66c170acfcb031967efc8740293f
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-17 11:34:40 +0530

    Time: 1 ms (100.00%) | Memory: 46 MB (68.48%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -2,33 +2,36 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int len1=nums1.length;
         int len2=nums2.length;
-        if(len1>len2) return findMedianSortedArrays(nums2,nums1);
+        if(len1>len2){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        int n=(len1+len2+1)/2;
         
-        int half=(len1+len2+1)/2;
-        int l=0;
-        int r=len1;
-        
-        while(l<=r){
-            int mid1=(l+r)/2;
-            int mid2=half-mid1;
-            
+        int start=0;
+        int end=len1;
+        while(start<=end){
+            int mid1=(start+end)/2;
+            int mid2=n-mid1;
             int l1=mid1>0?nums1[mid1-1]:Integer.MIN_VALUE;
             int l2=mid2>0?nums2[mid2-1]:Integer.MIN_VALUE;
             int r1=mid1<len1?nums1[mid1]:Integer.MAX_VALUE;
             int r2=mid2<len2?nums2[mid2]:Integer.MAX_VALUE;
-            
+
             if(l1<=r2 && l2<=r1){
-                //even case 
-               if((len1+len2)%2==0) return (double)                     (Math.max(l1,l2)+Math.min(r1,r2))/2;
-                else return Math.max(l1,l2);
+                if((len1+len2)%2==0){
+                    return ((double)Math.max(l1,l2)+(double)Math.min(r1,r2))/2;
+
+                }
+                else{
+                    return (double)Math.max(l1,l2);
+                }
             }
-            else if(l1<r2){
-               l=mid1+1;
+            else if(l1<=r2){
+                start=mid1+1;
             }
             else{
-             r=mid1-1;
+                end=mid1-1;
             }
-
         }
         return -1;
     }
