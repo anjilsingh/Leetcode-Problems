e146e1ae4701affdf2abde2f7f3149ae4fe2e0f0
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-20 10:05:55 +0530

    Time: 21 ms (52.26%) | Memory: 45.4 MB (33.80%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,29 +1,30 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int max=piles[0];
-        for(int i=0;i<piles.length;i++){
-            if(piles[i]>max) max=piles[i];
+        for(int i=1;i<piles.length;i++){
+            if(piles[i]>max){
+                max=piles[i];
+            }
         }
-        
-        //now linear serach from 1 tp max
-        int ans=max;
+
         int low=0;
         int high=max;
+        int ans=max;
+
         while(low<=high){
             int mid=(low+high)/2;
-            int  total=0;
-            for(int j=0;j<piles.length;j++){
-              total+=Math.ceil((double)piles[j]/mid);
+            double req=0;
+            for(int i=0;i<piles.length;i++){
+               req+=Math.ceil((double)piles[i]/mid);
             }
-            if(total<=h){
-              ans=mid;
-             high=mid-1;
+            if(req>h){
+                low=mid+1;
             }
             else{
-              low=mid+1;
+                ans=mid;
+                high=mid-1;
             }
         }
         return ans;
-        
     }
 }
