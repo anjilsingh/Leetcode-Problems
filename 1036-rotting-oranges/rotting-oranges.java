5aa3a88f4746cb07d39f6bbd574c264370ea8199
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-21 20:57:07 +0530

    Time: 1 ms (100.00%) | Memory: 42.9 MB (21.85%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,53 +1,48 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        //first check if grid is null or empty
-        if(grid==null || grid.length==0){
-           return 0;
-        }
         Queue<int[]>q=new LinkedList<>();
-        int r=grid.length;
-        int c=grid[0].length;
-        int checkCount=0;
-        for(int i=0;i<r;i++){
-         for(int j=0;j<c;j++){
-             if(grid[i][j]==2){
-               q.add(new int[]{i,j});
-             }
-             if(grid[i][j]!=0){
-                 checkCount++;
-             }
-         }
+        int row=grid.length-1;
+        int col=grid[0].length-1;
+        int fresh=0;
+        for(int i=0;i<=row;i++){
+            for(int j=0;j<=col;j++){
+              if(grid[i][j]==1) fresh++;
+
+              if(grid[i][j]==2){
+                q.add(new int[]{i,j});
+              }
+            }
         }
-        
-        if(checkCount==0) return 0;
-        int count=0;
-        int minCount=0;
+        int time=0;
         int[] dx={-1,1,0,0};
         int[] dy={0,0,-1,1};
-        while(!q.isEmpty()){
+
+        while(fresh>0 && !q.isEmpty()){
             int size=q.size();
-            count+=size;
+
             for(int i=0;i<size;i++){
-               int[] node=q.poll();
-               int row=node[0];
-               int col=node[1];
+                int[] node=q.poll();
+                int r=node[0];
+                int c=node[1];
+
                 for(int j=0;j<4;j++){
-                   int newRow=row+dx[j];
-                   int newCol=col+dy[j];
-                   if(newRow<0 ||newRow>=r ||newCol<0||newCol>=c||grid[newRow][newCol]==2||grid[newRow][newCol]==0) continue;
-                    
-                    grid[newRow][newCol]=2;
-                    q.add(new int[]{newRow,newCol});
+                   int  newR=r+dx[j];
+                   int  newC=c+dy[j];
+
+                    if(newR<0 ||newR>row||newC<0||newC>col||grid[newR][newC]==0||grid[newR][newC]==2){
+                        continue;
+                    }
+                    else{
+                        grid[newR][newC]=2;
+                        q.add(new int[]{newR, newC});
+
+                        fresh--;
+                    }
                 }
-            
-            
             }
-            if(q.size()!=0){
-              minCount++;
-            }
-        
+            time++;
         }
-        return count==checkCount?minCount:-1;
-        
+
+        return fresh==0?time:-1;
     }
 }
