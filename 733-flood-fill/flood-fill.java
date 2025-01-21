d098096af4e3d3bffed911ecda8b4cdd3e25810c
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-21 21:13:05 +0530

    Time: 1 ms (44.51%) | Memory: 44.6 MB (94.82%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,32 +1,39 @@
 class Solution {
-    public  void dfs(int[]dx, int[] dy,int inicolor,int color,int[][] image,int sr,int sc,int r,int c){
-        
-        image[sr][sc]=color;
-        for(int j=0;j<4;j++){
-
-              int newR=sr+dx[j];
-              int newC=sc+dy[j];
-               
-               if(newR<0 ||newR>=r ||newC<0 ||newC>=c||image[newR][newC]!=inicolor){
-                 continue;
-               }
-              dfs(dx,dy,inicolor,color,image,newR,newC,r,c);
-        
-        }
-    
-    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int r=image.length;
-        int c=image[0].length;
-        int inicolor=image[sr][sc];
-         if (inicolor == color) {
-            return image;
+        int[][] mat=new int[image.length][image[0].length];
+
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                mat[i][j]=image[i][j];
+            }
         }
+
+        int iniCol=image[sr][sc];
         int[] dx={-1,1,0,0};
         int[] dy={0,0,-1,1};
-        dfs(dx,dy,inicolor,color,image,sr,sc,r,c);
-        return image;
-        
-        
+
+        Queue<int[]>q=new LinkedList<>();
+        q.add(new int[]{sr,sc});
+        mat[sr][sc]=color;
+        while(!q.isEmpty()){
+             int[] node=q.poll();
+             int r=node[0];
+             int c=node[1];
+
+             for(int i=0;i<4;i++){
+                int newR=r+dx[i];
+                int newC=c+dy[i];
+
+                if(newR<0|| newR>=image.length||newC<0 ||newC>=image[0].length||image[newR][newC]!=iniCol ||mat[newR][newC]==color){
+                    continue;
+
+                }
+                else{
+                    mat[newR][newC]=color;
+                    q.add(new int[]{newR,newC});
+                }
+             }
+        }
+        return mat;
     }
 }
