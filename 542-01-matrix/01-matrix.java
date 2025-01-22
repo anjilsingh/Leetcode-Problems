ff6b73ce163be776768520bf2b6cdc15af365a7e
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-22 08:59:05 +0530

    Time: 13 ms (84.74%) | Memory: 47.6 MB (34.62%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,55 +1,62 @@
-class Solution {
-    class Node {
-    int first;
-    int second;
-    int third; 
-    Node(int _first, int _second, int _third) {
-        this.first = _first; 
-        this.second = _second; 
-        this.third = _third; 
+class Pair{
+    int r;
+    int c;
+    int dis;
+    Pair(int r,int c,int dis){
+        this.r=r;
+        this.c=c;
+        this.dis=dis;
     }
 }
+class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int row=mat.length;
-        int col=mat[0].length;
-       int[][]dis=new int[row][col];
-       int[][]vis=new int[row][col];
-        
-       Queue<Node>q=new LinkedList<>();
-        for(int i=0;i<row;i++){
-           for(int j=0;j<col;j++){
-            
-               if(mat[i][j]==0){
-                  dis[i][j]=0;
-                 q.add(new Node(i,j,0));
-                 vis[i][j]=1;
-             
-              }
-               else{
-                vis[i][j]=0;
-               }
-           }
+        int[][] ans=new int[mat.length][mat[0].length];
+        boolean[][] visited = new boolean[mat.length][mat[0].length];
+
+        // for(int i=0;i<mat.length;i++){
+        //     for(int j=0;j<mat[0].length;j++){
+        //         ans[i][j]=mat[i][j];
+        //     }
+        // }
+
+        Queue<Pair>q=new LinkedList<>();
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(mat[i][j]==0){
+                    q.add(new Pair(i,j,0));
+                    visited[i][j]=true;
+                }
+            }
         }
+
         int[] dx={-1,1,0,0};
         int[] dy={0,0,-1,1};
-        while(!q.isEmpty()){
-            
-              int r=q.peek().first;
-              int c=q.peek().second;
-              int steps=q.peek().third;
-              q.remove();
-              dis[r][c]=steps;
-              for(int j=0;j<4;j++){
-                  int newr=r+dx[j];
-                  int newc=c+dy[j];
-         if(newr<0 ||newr>=row ||newc<0 ||newc>=col||vis[newr][newc]==1)continue;
-                  
-                  q.add(new Node(newr,newc,steps+1));
-                  vis[newr][newc]=1;
-              }
-        }
-        return dis;
 
-    
+        while(!q.isEmpty()){
+            int size=q.size();
+
+            for(int i=0;i<size;i++){
+                Pair pair=q.poll();
+                int row=pair.r;
+                int col=pair.c;
+                int distance=pair.dis;
+
+                for(int j=0;j<4;j++){
+                    int newR=row+dx[j];
+                    int newC=col+dy[j];
+
+                    if(newR<0 ||newR>=mat.length||newC<0 ||newC>=mat[0].length||visited[newR][newC]==true) continue;
+
+                    else{
+                        ans[newR][newC]=distance+1;
+                        q.add(new Pair(newR,newC,distance+1));
+                        visited[newR][newC]=true;
+                    }
+                }
+
+            }
+        }
+        return ans;
+
     }
 }
