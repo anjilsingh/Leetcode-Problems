30db64ba17327f0e02ac541b269cf3346386b63a
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-26 11:52:09 +0530

    Time: 1 ms (74.15%) | Memory: 45.6 MB (65.95%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,38 +1,50 @@
-class Pair{
-    int i;
-    int j;
-    Pair(int i,int j){
-        this.i=i;
-        this.j=j;
-    }
-}
 class Solution {
     public void setZeroes(int[][] matrix) {
-        //can i use a hashmap to store all zero index
         
-        HashMap<Pair,Integer>mp=new HashMap<>();
+        //first try to put first row and first col as that extra arr which we have used for that ques
+
+        boolean fRow=false;
+        boolean fCol=false;
 
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-              if(matrix[i][j]==0){
-                mp.put(new Pair(i,j),0);
-              }
+
+                if(matrix[i][j]==0){
+                    if(i==0){
+                        fRow=true;
+                    }
+                    if(j==0){
+                        fCol=true;
+                    }
+                    //mark first row and first col as zero
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+
+                }
             }
         }
 
-         for (Pair key : mp.keySet()) {
-            int row = key.i;
-            int col = key.j;
+       //iterate in interna matrix and set 
 
-            // Set entire row to zero
-            for (int j = 0; j < matrix[0].length; j++) {
-                matrix[row][j] = 0;
-            }
-
-            // Set entire column to zero
-            for (int i = 0; i < matrix.length; i++) {
-                matrix[i][col] = 0;
+       for(int i=1;i<matrix.length;i++){
+        for(int j=1;j<matrix[0].length;j++){
+            if(matrix[0][j]==0 ||matrix[i][0]==0){
+                matrix[i][j]=0;
             }
         }
+       } 
+
+       //finally just make first row and first col zero 
+
+       if(fRow){
+         for(int i=0;i<matrix[0].length;i++){
+            matrix[0][i]=0;
+         }
+       }
+       if(fCol){
+        for(int i=0;i<matrix.length;i++){
+            matrix[i][0]=0;
+        }
+       }
     }
 }
