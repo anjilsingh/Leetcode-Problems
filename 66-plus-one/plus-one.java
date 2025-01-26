b7c236f81668520942be30ac21e37efc787f4e26
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-26 13:25:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (65.58%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,20 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-
-        // Traverse the array from the last digit
         for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
-            // If digit is 9, set it to 0
             digits[i] = 0;
         }
-
-        // If we are here, it means all the digits were 9
-        int[] newNumber = new int[n + 1];
-        newNumber[0] = 1; // Set the most significant digit to 1
-        return newNumber;
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
     }
 }
