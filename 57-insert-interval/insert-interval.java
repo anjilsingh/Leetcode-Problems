a695706b39c12c93e1db167a1402d9836510d04c
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-26 08:02:27 +0530

    Time: 1 ms (98.94%) | Memory: 45.1 MB (41.90%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -11,7 +11,7 @@ class Solution {
         }
 
         // Merge all overlapping or touching intervals
-        while (i < n && intervals[i][0] <= newInterval[1]) {
+        while (i < n && newInterval[1]>=intervals[i][0]  ) {
             newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
             newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             i++;
