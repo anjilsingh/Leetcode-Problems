359c2e66bf98107a3d96e84fa49fcacbe8bb1e86
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-26 11:15:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (79.16%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,47 +1,41 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> ans = new ArrayList<>();
-        
-        // Handle edge case when matrix is empty
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return ans;
-        }
+        List<Integer>al=new ArrayList<>();
+        int left=0;
+        int right=matrix[0].length-1;
+        int top=0;
+        int bottom=matrix.length-1;
 
-        int row = matrix.length;
-        int col = matrix[0].length;
-        int left = 0, right = col - 1;
-        int top = 0, bottom = row - 1;
+        while(left<=right && top<=bottom){
 
-        while (left <= right && top <= bottom) {
-            // Traverse from left to right along the top row
-            for (int i = left; i <= right; i++) {
-                ans.add(matrix[top][i]);
+            //print from left to right
+
+            for(int i=left;i<=right;i++){
+                al.add(matrix[top][i]);
             }
-            top++; // Move the top boundary down
+            top++;
 
-            // Traverse from top to bottom along the right column
-            for (int i = top; i <= bottom; i++) {
-                ans.add(matrix[i][right]);
+            //print from top to bottom 
+            for(int i=top;i<=bottom;i++){
+                al.add(matrix[i][right]);
             }
-            right--; // Move the right boundary left
+            right--;
 
-            if (top <= bottom) {
-                // Traverse from right to left along the bottom row
-                for (int i = right; i >= left; i--) {
-                    ans.add(matrix[bottom][i]);
-                }
-                bottom--; // Move the bottom boundary up
+            //print from right to left 
+            if(top<=bottom){
+            for(int i=right;i>=left;i--){
+                al.add(matrix[bottom][i]);
+            }
+            bottom--;
             }
 
-            if (left <= right) {
-                // Traverse from bottom to top along the left column
-                for (int i = bottom; i >= top; i--) {
-                    ans.add(matrix[i][left]);
-                }
-                left++; // Move the left boundary right
+            if(left<=right){
+            for(int i=bottom;i>=top;i--){
+                al.add(matrix[i][left]);
+            }
+            left++;
             }
         }
-
-        return ans;
+    return al;
     }
 }
