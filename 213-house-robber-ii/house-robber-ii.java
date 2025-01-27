55625cfe2ed2641c142badc60f5e85f2b7d409c6
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-27 18:51:24 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (63.87%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,22 +1,34 @@
 class Solution {
-    public int solve(int[] nums ,int i, int n, int[] dp){
-        if(i>=n){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        return dp[i]=Math.max(nums[i]+solve(nums,i+2,n,dp),solve(nums,i+1,n,dp));
-    }
-    public int rob(int[] nums) {
+    public int robb(int[] nums) {
         int n=nums.length;
         if(n==1){
             return nums[0];
         }
         int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        int[] dp1=new int[n];
-        Arrays.fill(dp1,-1);
-        return Math.max(solve(nums,0,n-1,dp),solve(nums,1,n,dp1));
+        dp[0]=nums[0];
+        //dp[1]=Math.max(dp[0],dp[1]);
+
+        for(int i=1;i<n;i++){
+            if(i==1){
+                dp[i]=Math.max(nums[0],nums[1]);
+            }
+            else{
+            dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
+            }
+        }
+        return dp[n-1];
+    }
+    public int rob(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
+        }
+         int[] skipFirst=new int[nums.length-1];
+         int[] skipLast=new int[nums.length-1];
+
+         for(int i=0;i<nums.length-1;i++){
+            skipFirst[i]=nums[i+1];
+            skipLast[i]=nums[i];
+         }
+         return Math.max(robb(skipFirst),robb(skipLast));
     }
 }
