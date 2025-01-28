555c8eb87dc8c2a211b2e3716c20657ea6689f82
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-28 11:11:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (79.21%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,21 +1,38 @@
 class Solution {
-     public int solve(int i,int n,int[] nums, int[] dp){
-        if(i>=n) return 0;
- if(dp[i]!=-1){
-    return dp[i];
- }
-        return dp[i]=Math.max(nums[i]+solve(i+2,n,nums,dp),solve(i+1,n,nums,dp));
+     public int robb(int[] nums) {
+        int n=nums.length;
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return nums[0];
+        }
+        int[] dp=new int[n];
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            if(i==1){
+                dp[i]=Math.max(nums[i],nums[i-1]);
+            }
+            else{
+                dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
+            }
+        }
+        return dp[n-1];
     }
 
     public int rob(int[] nums) {
         int n=nums.length;
-        if(n==1){
+        
+           if (n == 1) {
             return nums[0];
         }
-       int[] dp1=new int[n+1];
-       Arrays.fill(dp1,-1);
-          int[] dp2=new int[n+1];
-       Arrays.fill(dp2,-1);
-        return Math.max(solve(0,n-1, nums,dp1),solve(1,n,nums,dp2));
+        int[] num1=new int[n-1];
+        int[] num2=new int[n-1];
+        for(int i=0;i<n-1;i++){
+            num1[i]=nums[i+1];
+            num2[i]=nums[i];
+        }
+
+        return Math.max(robb(num1),robb(num2));
     }
 }
