09d19435f40bff73a9a366ad7eca675113687eea
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-01-30 04:51:55 +0530

    Time: 14 ms (96.99%) | Memory: 75.9 MB (5.03%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,49 +1,30 @@
 class Solution {
-    
-      static Boolean isSubsetSum(int N, int arr[], int sum){
-       boolean[][] dp=new boolean[N+1][sum+1];
-       //base cases
-       //if sum==0 true
-       //if n==0 return false 
- //apart from this we have two options either to inlcude or 
- //not and if the value is greater then summ then only not include
- 
-      for(int i=0;i<=N;i++){
-          for(int j=0;j<=sum;j++){
-              if(i==0 && j==0){
-                  dp[i][j]=true;
-              }
-              else if(i==0){
-                dp[i][j] =false;
-              }
-              else if(j==0){
-                  dp[i][j]=true;
-              }
-              else{
-                  if(arr[i-1]>j){
-                      dp[i][j]=dp[i-1][j];
-                  }
-                  else{
-                      dp[i][j]=dp[i-1][j] || dp[i-1][j-arr[i-1]];
-                  }
-              }
-              
-          }
-      }
-      return dp[N][sum];
-       
+    public boolean solve(int[] nums, int n, int target,Boolean[][] dp){
+        if(target==0) return true;
+        if(n==0) return false;
+        if(dp[n][target]!=null){
+            return dp[n][target];
+        }
+        if(nums[n-1]<=target){
+            return dp[n][target]= solve(nums,n-1,target-nums[n-1],dp)||solve(nums,n-1,target,dp);
+        }
+        else{
+            return dp[n][target]= solve(nums,n-1,target,dp);
+        }
     }
-    
     public boolean canPartition(int[] nums) {
         int n=nums.length;
         int sum=0;
         for(int i=0;i<n;i++){
             sum+=nums[i];
         }
-        if(sum%2==0){
-           return isSubsetSum(n,nums,sum/2);
-        }
         
-        return false;
+        if(sum%2==0){
+            Boolean[][] dp=new Boolean[n+1][sum+1];
+            return solve(nums,n,sum/2,dp);
+        }
+        else{
+            return false;
+        }
     }
 }
