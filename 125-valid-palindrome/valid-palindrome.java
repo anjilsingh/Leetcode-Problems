7a9b9af3906504d906b2572147d9e6c1a1b660a1
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-02-05 21:41:04 +0530

    Time: 3 ms (74.42%) | Memory: 42.7 MB (95.06%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,28 +1,24 @@
 class Solution {
-    public boolean aNum(char ch){
-        return ((ch>='a' && ch<='z') ||(ch>='A'&& ch<='Z')||(ch>='0' && ch<='9'));
+    public boolean isAlpha(char ch){
+        return ((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')|| (ch>='0' && ch<='9'));
     }
     public boolean isPalindrome(String s) {
-        
         int l=0;
         int r=s.length()-1;
-
         while(l<r){
-            
-            while(l<r && !aNum(s.charAt(l))){
+            while(l<r && !isAlpha(s.charAt(l))){
                 l++;
             }
-
-            while(r>l && !aNum(s.charAt(r))){
+            while(l<r && !isAlpha(s.charAt(r))){
                 r--;
             }
 
-            if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r))) return false;
-
+ if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase((s.charAt(r)))){
+                return false;
+            }
             l++;
             r--;
         }
         return true;
-
     }
 }
