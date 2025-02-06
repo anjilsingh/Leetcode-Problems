d79ae443fe2e5f3363bb266ef7edae993dbe8522
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-02-07 00:26:50 +0530

    Time: 81 ms (13.48%) | Memory: 60 MB (35.13%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,20 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int n=nums.length;
-        int[] output=new int[n-k+1];
-
-        PriorityQueue<int[]>maxHeap=new PriorityQueue<>((a,b)->b[1]-a[1]);
-         int j=0;
-        for(int i=0;i<n;i++){
-            maxHeap.add(new int[]{i,nums[i]});
-            if(i>=k-1){
-                while(maxHeap.peek()[0]<=i-k){
-                    maxHeap.poll();
+        int n = nums.length;
+        int[] output = new int[n - k + 1];
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]); // Max Heap
+        int x = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            pq.add(new int[]{i, nums[i]});
+            
+            // Ensure we have at least `k` elements before recording max
+            if (i >= k - 1) {
+                // Remove elements that are out of the current window
+                while (!pq.isEmpty() && pq.peek()[0] <= i - k) {
+                    pq.poll();
                 }
-                output[j++]=maxHeap.peek()[1];
+                
+                // Store the maximum element of the current window
+                output[x++] = pq.peek()[1];
             }
-           
         }
+        
         return output;
     }
 }
