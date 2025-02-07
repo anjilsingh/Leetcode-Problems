d1e5e498d40cb2c086429287bf0ed8e0cb816772
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-02-07 09:03:28 +0530

    Time: 21 ms (52.26%) | Memory: 44.8 MB (97.45%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,28 +1,31 @@
+import java.util.*;
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
+        //find the max and run a bs from 1 to max of element try to find the min possible4
         int max=piles[0];
-        for(int i=1;i<piles.length;i++){
+        int n=piles.length;
+        for(int i=1;i<n;i++){
             if(piles[i]>max){
-                max=piles[i];
+                 max=piles[i];
             }
         }
 
         int low=0;
         int high=max;
         int ans=max;
-
         while(low<=high){
             int mid=(low+high)/2;
-            double req=0;
+
+            double time=0;
             for(int i=0;i<piles.length;i++){
-               req+=Math.ceil((double)piles[i]/mid);
+                time+=Math.ceil((double)piles[i]/mid);
             }
-            if(req>h){
-                low=mid+1;
-            }
-            else{
+            if(time<=h){
                 ans=mid;
                 high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
         return ans;
