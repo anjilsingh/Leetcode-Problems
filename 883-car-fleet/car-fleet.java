a3bb0510b3ca6074cd4e66c8432cdd634d0c0723
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-02-07 08:34:49 +0530

    Time: 76 ms (64.75%) | Memory: 59.6 MB (18.28%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,19 +1,22 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
         ArrayList<int[]>al=new ArrayList<>();
-
         for(int i=0;i<position.length;i++){
             al.add(new int[]{position[i],speed[i]});
         }
         al.sort((a,b)->b[0]-a[0]);
 
-        Stack<Double>st=new Stack();
+        Stack<Double>st=new Stack<>();
 
-        for(int i=0;i<position.length;i++){
-            double time=(double)(target-al.get(i)[0])/al.get(i)[1];
+        for(int i=0;i<al.size();i++){
+            int[] curr=al.get(i);
+
+            double time=(double)(target-curr[0])/curr[1];
+
             if(!st.isEmpty() && st.peek()>=time){
                 continue;
             }
+
             st.push(time);
         }
         return st.size();
