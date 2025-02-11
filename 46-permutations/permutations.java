9bcde78c5edc941f1e71c55558573cf857d877ef
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-02-11 09:48:40 +0530

    Time: 1 ms (92.87%) | Memory: 44.6 MB (46.20%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,19 +1,21 @@
 class Solution {
-    public void solve(int[] nums,List<List<Integer>>res,List<Integer>curr){
-        if(curr.size()==nums.length){
-            res.add(new ArrayList<>(curr));
+    public void solve(int[] nums,List<List<Integer>>ans,int start,int n,List<Integer>curr) {
+        if(curr.size()==n){
+            ans.add(new ArrayList<>(curr));
+            return;
         }
-        for(int i=0;i<nums.length;i++){
+
+        for(int i=0;i<n;i++){
             if(curr.contains(nums[i])) continue;
             curr.add(nums[i]);
-            solve(nums,res,curr);
+            solve(nums,ans,i+1,n,curr);
             curr.remove(curr.size()-1);
-
         }
     }
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>>res=new ArrayList<>();
-        solve(nums,res,new ArrayList<>());
-        return res;
+        List<List<Integer>>ans=new ArrayList<>();
+        int n=nums.length;
+        solve(nums,ans,0,n,new ArrayList<>());
+        return ans;
     }
 }
