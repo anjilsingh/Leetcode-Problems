6b365ad24491fc6d34c9fa7cf5e870571d71e6e3
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-02-11 18:41:48 +0530

    Time: 130 ms (87.40%) | Memory: 41.4 MB (88.41%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,25 +1,24 @@
 class Solution {
-    int row;
-    int col;
-    public boolean find(char[][]board,String word,int i,int j,int index){
+    public boolean find(char[][]board, String word,int i ,int j,int index){
         if(index==word.length()){
             return true;
         }
-        if(i<0||i>=row||j<0||j>=col||board[i][j]!=word.charAt(index)||board[i][j]=='$'){
+        if(i<0|| i>=board.length ||j<0 ||j>=board[i].length||board[i][j]!=word.charAt(index) || board[i][j]=='$'){
             return false;
         }
+
         board[i][j]='$';
-        boolean res=(find(board,word,i,j+1,index+1)||find(board,word,i,j-1,index+1)||find(board,word,i-1,j,index+1)||find(board,word,i+1,j,index+1));
+       boolean res=(find(board,word,i,j+1,index+1)||find(board,word,i,j-1,index+1)||find(board,word,i-1,j,index+1)||find(board,word,i+1,j,index+1));
         board[i][j]=word.charAt(index);
         return res;
     }
     public boolean exist(char[][] board, String word) {
-        row=board.length;
-        col=board[0].length;
+        int row=board.length;
+        int col=board[0].length;
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(board[i][j]==word.charAt(0)){
-                    if(find(board,word,i,j,0)){
+                    if(find(board,word,i,j,0)==true){
                         return true;
                     }
                 }
