54ab0f6a8db783662b6d8e4e4995623ee5db8bb8
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-02-11 09:39:09 +0530

    Time: 4 ms (43.32%) | Memory: 43.3 MB (52.74%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,24 +1,28 @@
 class Solution {
-    public void solve(int[] candidates,List<List<Integer>>res,List<Integer>curr,int start,int sum,int target){
-        if(sum==target){
-            res.add(new ArrayList<>(curr));
-            return;
-        }
-        if(sum>target){
-            return;
-        }
-        for(int i=start;i<candidates.length;i++){
-            if(i>start && candidates[i]==candidates[i-1]) continue;
-            curr.add(candidates[i]);
-            solve(candidates,res,curr,i+1,sum+candidates[i],target);
-            curr.remove(curr.size()-1);
-        }
+    public void solve(int[] candidates,int start,int sum,int n,List<List<Integer>>ans,int target,List<Integer>curr){
 
+      if(sum==target){
+        ans.add(new ArrayList<>(curr));
+        return;
+      }
+
+      if(sum>target){
+        return;
+
+      }
+
+      for(int i=start;i<n;i++){
+        if(i>start && candidates[i]==candidates[i-1]) continue;
+        curr.add(candidates[i]);
+        solve(candidates,i+1,sum+candidates[i],n,ans,target,curr);
+        curr.remove(curr.size()-1);
+      }
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>>ans=new ArrayList<>();
+        int n=candidates.length;
         Arrays.sort(candidates);
-        List<List<Integer>>res=new ArrayList<>();
-        solve(candidates,res,new ArrayList<>(),0,0,target);
-        return res;
+        solve(candidates,0,0,n,ans,target,new ArrayList<>());
+        return ans;
     }
 }
