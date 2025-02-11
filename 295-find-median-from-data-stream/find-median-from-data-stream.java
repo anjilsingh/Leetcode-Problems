410300b0910b74d768acd6a7de2444dfb96be6a5
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-02-11 09:18:13 +0530

    Time: 92 ms (77.23%) | Memory: 63.4 MB (87.91%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,39 +1,41 @@
-//we have to create two pq 
-//one for adding all min value that will be maxheap
-//one for adding all max value that will be minheap
-//then make sure while adding remove and push to other if diff is greater 
-//then if size is same remove from bothe side and then divide by two 
-//is small length is greater return its peak else return large peak
 class MedianFinder {
     PriorityQueue<Integer>small;
-    PriorityQueue<Integer>large;
+    PriorityQueue<Integer>big;
     public MedianFinder() {
         small=new PriorityQueue<>((a,b)->b-a);
-        large=new PriorityQueue<>((a,b)->a-b);
+        big=new PriorityQueue<>((a,b)->a-b);
     }
     
     public void addNum(int num) {
-    // Add to small heap first
-    small.add(num);
+        small.add(num);
+        //small peek val should be less than big peek val
+        if(!big.isEmpty() && small.peek()>big.peek()){
+            big.add(small.poll());
+        }
 
-    // Ensure small's top is <= large's top
-    if (!large.isEmpty() && small.peek() > large.peek()) {
-        large.add(small.poll());
+        //if diff bts small and big increase by one
+
+        if(small.size()>big.size()+1){
+            big.add(small.poll());
+        }
+
+        if(big.size()>small.size()+1){
+            small.add(big.poll());
+        }
     }
-
-    // Rebalance the heaps if sizes differ by more than 1
-    if (small.size() > large.size() + 1) {
-        large.add(small.poll());
-    } else if (large.size() > small.size() + 1) {
-        small.add(large.poll());
-    }
-}
-
     
     public double findMedian() {
-        if(large.size()==small.size()) return ((double)(large.peek())+(double)(small.peek()))/2;
-        else if(large.size()>small.size())  return (double)large.peek();
-        else return (double) small.peek();
+        if(big.size()==small.size()){
+            return (((double)big.peek()+small.peek())/2);
+
+        }
+
+        else if(big.size()>small.size()){
+            return big.peek();
+        }
+        else{
+            return small.peek();
+        }
     }
 }
 
