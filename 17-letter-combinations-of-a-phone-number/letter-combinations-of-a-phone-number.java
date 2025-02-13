beac3d829381173afc32893686e72dea0e0aed74
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-02-13 09:42:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (94.92%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,33 +1,39 @@
 class Solution {
-    HashMap<Character,String>mp=new HashMap<>();
-    {
-    mp.put('2',"abc");
-    mp.put('3',"def");
-    mp.put('4',"ghi");
-    mp.put('5',"jkl");
-    mp.put('6',"mno");
-    mp.put('7',"pqrs");
-    mp.put('8',"tuv");
-    mp.put('9',"wxyz");
-    }
-    public void solve(List<String>res,String digits,int index,StringBuilder sb){
-        if(index>=digits.length()){
-            res.add(sb.toString());
-            return;
-        }
-        String word=mp.get(digits.charAt(index));
-        for(int i=0;i<word.length();i++){
-            sb.append(word.charAt(i));
-            solve(res,digits,index+1,sb);
-            sb.deleteCharAt(sb.length()-1);
+       HashMap<Character,String>mp=new HashMap<>();
+        {
+            mp.put('2',"abc");
+            mp.put('3',"def");
+            mp.put('4',"ghi");
+            mp.put('5',"jkl");
+            mp.put('6',"mno");
+            mp.put('7',"pqrs");
+            mp.put('8',"tuv");
+            mp.put('9',"wxyz");
+
         }
 
-    }
+        public void solve(StringBuilder sb,String digits,int index,List<String>res){
+            if(index>=digits.length()){
+                res.add(sb.toString());
+                return;
+            }
+
+            String word=mp.get(digits.charAt(index));
+
+            for(int i=0;i<word.length();i++){
+                sb.append(word.charAt(i));
+                solve(sb,digits,index+1,res);
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
     public List<String> letterCombinations(String digits) {
-        List<String>res=new ArrayList<>();
-        StringBuilder sb=new StringBuilder();
-        if(digits.length()==0) return res;
-        solve(res,digits,0,sb);
+     List<String>res=new ArrayList<>();
+     if(digits.length()==0){
         return res;
+     }
+     StringBuilder sb=new StringBuilder();
+     solve(sb,digits,0,res);
+     return res;
+
     }
 }
