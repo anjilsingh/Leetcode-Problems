c61c0dbdde01847006599703ce9de883e90a2fec
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-02-13 21:13:24 +0530

    Time: 1 ms (99.73%) | Memory: 44.7 MB (23.18%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,25 +1,21 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n=triangle.size()-1;
-        int[][] dp=new int[n][n];
-        for(int[] arr:dp){
-            Arrays.fill(arr,-1);
-        }
-        return memoziation(0,0,triangle,dp);
-
-    }
-
-    public int memoziation(int i,int j,List<List<Integer>>triangle,int[][]dp) {
-          if(i==triangle.size()-1){
+    public int solve(int i,int j,List<List<Integer>>triangle,int[][] dp){
+        if(i==triangle.size()-1){
             return triangle.get(i).get(j);
+        }
 
-          }
-          if(dp[i][j]!=-1){
-            return dp[i][j];
-          }
-          int down=memoziation(i+1,j,triangle,dp);
-          int diag=memoziation(i+1,j+1,triangle,dp);
+        if(dp[i][j]!=-1) return dp[i][j];
+        int down=solve(i+1,j,triangle,dp);
+        int right=solve(i+1,j+1,triangle,dp);
 
-          return dp[i][j]=triangle.get(i).get(j)+Math.min(down,diag);
+        return dp[i][j]=triangle.get(i).get(j)+Math.min(down,right);
+    }
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n=triangle.size();
+        int[][] dp=new int[n+1][n+1];
+        for(int[] r:dp){
+            Arrays.fill(r,-1);
+        }
+        return solve(0,0,triangle,dp);
     }
 }
