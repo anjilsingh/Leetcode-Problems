f7c883e5b505485720d50d73249c532d7f7d5f9b
Anjil Singh <159554637+Strivinggirl@users.noreply.github.com>
2025-02-13 10:07:15 +0530

    Time: 3 ms (51.12%) | Memory: 45.4 MB (9.49%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,46 +1,54 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>>ans=new ArrayList<>();
-        char[][] board=new char[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                board[i][j]='.';
-            }
+        List<List<String>>res=new ArrayList<>();
+       char[][] board=new char[n][n];
+       for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            board[i][j]='.';
         }
-        solve(0,board,ans);
-        return ans;
+       }  
+       solve(0,res,board);
+       return res;
+
+
     }
 
-    public void solve(int r,char[][]board,List<List<String>>ans){
-        if(r==board.length){
-            List<String>copy=new ArrayList<>();
-            for(char[] currRow:board){
-                copy.add(new String(currRow));
+    public void solve(int row,List<List<String>>res,char[][] board){
+        if(row==board.length){
+            List<String>curr=new ArrayList<>();
+            for(char[] r:board){
+                curr.add(new String(r));
+               
             }
-            ans.add(copy);
-            return;
+          res.add(curr);
+          return;
         }
-        for(int i=0;i<board.length;i++){
-            if(isValid(board,r,i)){
-                board[r][i]='Q';
-                solve(r+1,board,ans);
-                board[r][i]='.';
-            }
-        }
-    }
-    public boolean isValid(char[][]board,int r,int c){
-        //check foward above
 
-        for(int i=r-1;i>=0 ;i--){
-            if(board[i][c]=='Q') return false;
+        for(int j=0;j<board.length;j++){
+            if(isValid(board,row,j)){
+                board[row][j]='Q';
+                solve(row+1,res,board);
+                board[row][j]='.';
+            }
         }
-        //chek diagonal
-        for(int i=r-1,j=c-1;i>=0 &&j>=0;i--,j--){
-            if(board[i][j]=='Q') return false;
-        }
-        for(int i=r-1 , j=c+1 ;i>=0 &&j<board[0].length;i--,j++){
-            if(board[i][j]=='Q') return false;
-        }
-        return true;
     }
+
+   public boolean isValid(char[][] board, int r, int c) {
+    // Check for any queen in the same column above
+    for (int i = r - 1; i >= 0; i--) {
+        if (board[i][c] == 'Q') return false;
+    }
+
+    // Check for upper-left diagonal
+    for (int i = r - 1, j = c - 1; i >= 0 && j >= 0; i--, j--) {
+        if (board[i][j] == 'Q') return false;
+    }
+
+    // Check for upper-right diagonal
+    for (int i = r - 1, j = c + 1; i >= 0 && j < board.length; i--, j++) {
+        if (board[i][j] == 'Q') return false;
+    }
+
+    return true; // If no conflict, return true
+}
 }
