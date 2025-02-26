8c4640bf44a62aa057ac6aa1edb76d8bf233d846
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-02-26 21:34:20 +0530

    Time: 7 ms (66.24%) | Memory: 47.8 MB (81.68%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,15 +1,16 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
        
-        HashMap<String,ArrayList<String>>mp=new HashMap<>();
-        for(String str:strs){
-           char[] strChar=str.toCharArray();
-            Arrays.sort(strChar);
-             String key=Arrays.toString(strChar);
-            mp.putIfAbsent(key,new ArrayList<>());
-            mp.get(key).add(str);
-        }
-        return new ArrayList<>(mp.values());
+           HashMap<String,List<String>>mp=new HashMap<>();
+           for(String str:strs){
+                  char stChar[]=str.toCharArray();
+                  Arrays.sort(stChar);
+                  String key=new String(stChar);
+                  
+                  mp.putIfAbsent(key,new ArrayList<>());
+                  mp.get(key).add(str);
+            }
 
+        return new ArrayList<>(mp.values());
     }
 }
