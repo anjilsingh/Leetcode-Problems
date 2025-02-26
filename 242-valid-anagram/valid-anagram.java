61ee4dcd3378200c85fe34e2d5b2445d38883904
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-02-26 21:23:07 +0530

    Time: 5 ms (53.54%) | Memory: 42.7 MB (97.55%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] arr=new int[26];
+        //we can create a 26 size of array
         if(s.length()!=t.length()) return false;
+        int[] arr=new int[26];
         for(int i=0;i<s.length();i++){
             arr[s.charAt(i)-'a']++;
             arr[t.charAt(i)-'a']--;
         }
 
-        for(int i=0;i<26;i++)
-        {
+        for(int i=0;i<26;i++){
             if(arr[i]!=0){
                 return false;
             }
