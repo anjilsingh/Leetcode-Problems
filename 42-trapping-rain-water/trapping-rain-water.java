93cdb99493df7392af8a9f6e4b54ba4f0be027ab
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-02-27 06:48:09 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (13.43%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,40 +1,31 @@
 class Solution {
     public int trap(int[] height) {
-       //lets do it by two poninter approach
-        
-    //so which is the deciding factor 
-        //we can find the value from both side lefftmax and rightmax
-        //from leess side value we can easily find further 
-        
-        
         int leftMax=0;
         int rightMax=0;
         int left=0;
         int right=height.length-1;
-        int amount=0;
+        int ans=0;
         while(left<=right){
             if(leftMax<=rightMax){
-                if(height[left]<leftMax){
-                    amount+=leftMax-height[left];
-                    
+                if(leftMax>height[left]){
+                  ans+=leftMax-height[left];
                 }
                 else{
-                    
                     leftMax=height[left];
                 }
                 left++;
             }
+
             else{
-                if(height[right]<rightMax){
-                    
-                    amount+=rightMax-height[right];
+                if(rightMax>height[right]){
+                    ans+=rightMax-height[right];
                 }
                 else{
-                rightMax=height[right];
+                    rightMax=height[right];
                 }
                 right--;
             }
         }
-        return amount;
+        return ans;
     }
 }
