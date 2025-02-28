4a611d94672066a0fee658bbb5d1d2a2209db163
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-02-28 21:14:27 +0530

    Time: 4 ms (97.15%) | Memory: 44.8 MB (67.50%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -2,22 +2,22 @@ class MinStack {
     Stack<Integer>st;
     Stack<Integer>minSt;
     public MinStack() {
-        st=new Stack<>();
-        minSt=new Stack<>();
+        st=new Stack();
+        minSt=new Stack();
     }
     
     public void push(int val) {
         st.push(val);
-        if(minSt.isEmpty() || minSt.peek()>=val){
+        if(minSt.isEmpty() ||minSt.peek()>=val){
             minSt.push(val);
         }
     }
     
     public void pop() {
         int val=st.pop();
-        if(minSt.peek()==val){
+         if(minSt.peek()==val){
             minSt.pop();
-        }
+         }
     }
     
     public int top() {
@@ -25,7 +25,7 @@ class MinStack {
     }
     
     public int getMin() {
-        return minSt.peek();
+         return minSt.peek();
     }
 }
 
