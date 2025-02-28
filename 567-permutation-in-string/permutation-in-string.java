53fbd08f3b33e3a6504316fdd341a704af99e462
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-02-28 20:41:29 +0530

    Time: 10 ms (45.35%) | Memory: 42.4 MB (94.79%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,51 +1,44 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        
-        //matches==26
-        if(s2.length()<s1.length()) return false;
-
+         if (s1.length() > s2.length()) return false;
         int[] s1arr=new int[26];
         int[] s2arr=new int[26];
-
         for(int i=0;i<s1.length();i++){
             s1arr[s1.charAt(i)-'a']++;
             s2arr[s2.charAt(i)-'a']++;
         }
-
         int matches=0;
         for(int i=0;i<26;i++){
             if(s1arr[i]==s2arr[i]){
                 matches++;
             }
-
         }
 
         int l=0;
         for(int r=s1.length();r<s2.length();r++){
             if(matches==26) return true;
+
             int index=s2.charAt(r)-'a';
             s2arr[index]++;
-
-            if(s2arr[index]==s1arr[index]){
+            if(s1arr[index]==s2arr[index]){
                 matches++;
             }
-            if(s2arr[index]==s1arr[index]+1){
+            else if(s1arr[index]+1==s2arr[index]){
                 matches--;
             }
 
+
             index=s2.charAt(l)-'a';
             s2arr[index]--;
-
-            if(s2arr[index]==s1arr[index]){
+            if(s1arr[index]==s2arr[index]){
                 matches++;
             }
-            if(s2arr[index]+1==s1arr[index]){
+            else if(s1arr[index]==s2arr[index]+1){
                 matches--;
             }
             l++;
 
         }
-return matches==26;
-
+        return matches==26;
     }
 }
