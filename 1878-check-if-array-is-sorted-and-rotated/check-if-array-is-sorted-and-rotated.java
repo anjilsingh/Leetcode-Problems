7429d4c058bd1cb9b5d9764a5a81d50725aa1350
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-03-01 09:21:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (51.48%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,18 +1,11 @@
 class Solution {
     public boolean check(int[] nums) {
-        int cnt = 0;
-        int n = nums.length;
-        
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                cnt++;
-            }
+        int cnt=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1]) cnt++;
         }
-        // If the last element is greater than the first, it's another breakpoint.
-        if (nums[n - 1] > nums[0]) {
-            cnt++;
-        }
-        
-        return cnt <= 1;
+        if(nums[nums.length-1]>nums[0]) cnt++;
+
+        return cnt<=1;
     }
 }
