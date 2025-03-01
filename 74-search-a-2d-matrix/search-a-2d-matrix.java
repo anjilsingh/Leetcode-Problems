c0c8119b7fb06e1ca70a680247b28d567dd1d122
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-03-01 07:00:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (37.84%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row=matrix.length-1;
-        int col=matrix[0].length-1;
+        
         int start=0;
-        int end=row;
+        int end=matrix.length-1;
+
         while(start<=end){
             int mid=(start+end)/2;
 
             if(target<matrix[mid][0]){
                 end=mid-1;
             }
-            else if(target>matrix[mid][col]){
+            else if(target>matrix[mid][matrix[0].length-1]){
                 start=mid+1;
             }
             else{
@@ -20,21 +20,24 @@ class Solution {
         if(start>end){
             return false;
         }
-        int r=(start+end)/2;
+
+        int row=(start+end)/2;
+
         int left=0;
-        int right=col;
-        while(left<=right)
-        {
+        int right=matrix[0].length-1;
+
+        while(left<=right){
             int mid=(left+right)/2;
-            if(matrix[r][mid]==target){
+            if(matrix[row][mid]==target){
                 return true;
             }
-            else if(matrix[r][mid]<target){
+            else if(matrix[row][mid]<target){
                 left=mid+1;
             }
             else{
                 right=mid-1;
             }
+
         }
         return false;
     }
