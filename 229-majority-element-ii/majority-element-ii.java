c29acb46ad00202e419a0abf70580bd1bba659b4
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-03-04 13:35:59 +0530

    Time: 10 ms (46.21%) | Memory: 47.8 MB (65.58%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,36 +1,16 @@
-import java.util.*;
-
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> ans = new ArrayList<>();
-        int n = nums.length;
-        
-        if (n == 1) {
-            ans.add(nums[0]);
-            return ans;
+        List<Integer>ans=new ArrayList<>();
+        HashMap<Integer,Integer>mp=new HashMap<>();
+        for(int num:nums){
+            mp.put(num,mp.getOrDefault(num,0)+1);
         }
-        
-        Arrays.sort(nums);  // Sort the array to group identical elements together
-        
-        int count = 1;
-        
-        for (int i = 1; i < n; i++) {
-            if (nums[i] == nums[i - 1]) {
-                count++;  // Count the occurrence of the current number
-            } else {
-                if (count > n / 3) {
-                    ans.add(nums[i - 1]);  // Add to result if count > n/3
-                }
-                count = 1;  // Reset the count for the new number
+
+        for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
+            if(entry.getValue()>nums.length/3){
+                ans.add(entry.getKey());
             }
         }
-        
-        // Handle the last element in the sorted array
-        if (count > n / 3) {
-            ans.add(nums[n - 1]);
-        }
-        
-        // Optional: Remove duplicates if needed (in case duplicates are not allowed)
         return ans;
     }
 }
