22d736baf5b6de2723261d15602bc43763b0c4a4
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-03-07 21:13:22 +0530

    Time: 2 ms (77.13%) | Memory: 44.9 MB (33.83%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,55 +1,64 @@
 class Solution {
-    public boolean isValid(int r,int c,char[][]board){
+    public boolean isValid(char[][] board,int row,int col){
         HashSet<Character>seen=new HashSet<>();
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-                char ch=board[r+i][c+j];
+                char ch=board[row+i][col+j];
                 if(ch=='.') continue;
-             
-                    if(seen.contains(ch)) return false;
-                    seen.add(ch);
-                
+                if(seen.contains(ch)){
+                    return false;
+                }
+                seen.add(ch);
             }
         }
         return true;
     }
     public boolean isValidSudoku(char[][] board) {
         
-        //we need to check for all rows
-        //for all cols 
-        //and for all 3*3 grid 
-
+        //validate all rows
         for(int i=0;i<9;i++){
-            HashSet<Character>set=new HashSet<>();
+            HashSet<Character>seen=new HashSet<>();
             for(int j=0;j<9;j++){
                 char ch=board[i][j];
-                if(ch=='.') continue;
-                
-                    if(set.contains(ch)) return false;
-                    set.add(ch);
-                
+                if(ch=='.') {
+                    continue;
+                }
+                else{
+                    if(seen.contains(ch)) return false;
+
+                    seen.add(ch);
+                }
             }
+
         }
 
-        for(int i=0;i<9;i++){
-            HashSet<Character>set=new HashSet<>();
+        //validate all cols
+           for(int i=0;i<9;i++){
+            HashSet<Character>seen=new HashSet<>();
             for(int j=0;j<9;j++){
-                    char ch=board[j][i];
-                     if(ch=='.') continue;
-                    if(set.contains(ch)) return false;
-                    set.add(ch);
-                
-            }
-        }
+                char ch=board[j][i];
+                if(ch=='.') {
+                    continue;
+                }
+                else{
+                    if(seen.contains(ch)) return false;
 
-        //now check for all 3*3
+                    seen.add(ch);
+                }
+            }
+            
+        }
+        //validate all 3*3 matrix
+
 
         for(int i=0;i<9;i=i+3){
             for(int j=0;j<9;j=j+3){
-                if(isValid(i,j,board)==false) return false;
+
+                if(isValid(board,i,j)==false){
+                    return false;
+                }
             }
         }
-
         return true;
     }
 }
