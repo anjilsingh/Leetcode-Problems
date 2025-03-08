7db956d343e5c990351a99590d10eb1afd014654
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-03-08 11:17:42 +0530

    Time: 22 ms (32.19%) | Memory: 45.7 MB (22.65%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,41 +1,42 @@
 class Solution {
     public String minWindow(String s, String t) {
-        //concept if need and have 
-        if (s == null || t == null || s.length() < t.length()) {
-            return "";
-        }
-        HashMap<Character,Integer>mpT=new HashMap<>();
-        for(int i=0;i<t.length();i++){
-            mpT.put(t.charAt(i),mpT.getOrDefault(t.charAt(i),0)+1);
-        }
-        int need=mpT.size();
-        int have=0;
-        int l=0;
-        int len=s.length()+1;
+        if(s.length()<t.length()) return "";
+        int minLength=Integer.MAX_VALUE;
         int[] index={-1,-1};
-        HashMap<Character,Integer>mpS=new HashMap<>();
-        for(int r=0;r<s.length();r++){
-            mpS.put(s.charAt(r),mpS.getOrDefault(s.charAt(r),0)+1);
-            if(mpT.containsKey(s.charAt(r)) && mpS.get(s.charAt(r)).equals(mpT.get(s.charAt(r)))){
-                have++;
-            }
-
-            while(have==need){
-                  if(r-l+1<len){
-                     len=r-l+1;
-                     index[0]=l;
-                     index[1]=r;
-                  }
-                  mpS.put(s.charAt(l),mpS.getOrDefault(s.charAt(l),0)-1);
-                 if(mpT.containsKey(s.charAt(l))){
-                 if(mpS.get(s.charAt(l))<mpT.get(s.charAt(l))){
-                    have--;
-                 }
-                 }
-                 l++;
-            }
+        HashMap<Character,Integer>tmp=new HashMap<>();
+        for(int i=0;i<t.length();i++){
+            tmp.put(t.charAt(i),tmp.getOrDefault(t.charAt(i),0)+1);
         }
-        if(len==s.length()+1) return "";
-        return s.substring(index[0],index[1]+1);
+        int need=tmp.size();
+        int have=0;
+        
+        HashMap<Character,Integer>smp=new HashMap<>();
+        int l=0;
+        for(int r=0;r<s.length();r++){
+           smp.put(s.charAt(r),smp.getOrDefault(s.charAt(r),0)+1);
+           if(tmp.containsKey(s.charAt(r)) && smp.get(s.charAt(r)).equals(tmp.get(s.charAt(r)))){
+            have++;
+           }
+
+
+           while(have==need){
+              if(r-l+1<minLength){
+                minLength=r-l+1;
+                index[0]=l;
+                index[1]=r;
+              }
+
+             smp.put(s.charAt(l), smp.getOrDefault(s.charAt(l), 0) - 1);
+
+     if(tmp.containsKey(s.charAt(l)) && smp.get(s.charAt(l))<tmp.get(s.charAt(l))){
+            have--;
+           }
+           l++;
+
+           }
+        }
+
+        return minLength==Integer.MAX_VALUE?"":s.substring(index[0],index[1]+1);
+        
     }
 }
