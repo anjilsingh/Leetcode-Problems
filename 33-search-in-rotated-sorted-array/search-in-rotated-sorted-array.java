d7e7d21277a8805971ba3beab89380d02c5d03c8
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-03-09 13:47:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (19.46%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,26 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
+        //we have to find the element in rotated sorted array 
         int left=0;
         int right=nums.length-1;
+
         while(left<=right){
-            int mid=(left+right)/2;
+            int mid=left+(right-left)/2;
             if(nums[mid]==target){
                 return mid;
             }
-            //left part
+
+            //either in left soted part 
             if(nums[left]<=nums[mid]){
-                if(target<nums[left] ||target>nums[mid]){
+                if(target<nums[left] || target>nums[mid]){
                     left=mid+1;
                 }
                 else{
                     right=mid-1;
                 }
             }
-            //right part
-            else{
-                if(target>nums[right] ||target<nums[mid]){
-                    right=mid-1;
 
+            
+
+            //or in right sorted part 
+            else{
+                if(target>nums[right] || target<nums[mid]){
+                    right=mid-1;
                 }
                 else{
                     left=mid+1;
