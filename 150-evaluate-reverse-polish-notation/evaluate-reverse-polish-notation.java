4131aa55a1377386bb14708d58c711cc50bee6ab
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-03-09 06:14:23 +0530

    Time: 6 ms (76.64%) | Memory: 44.5 MB (80.67%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,33 +1,32 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer>st=new Stack();
-
+        Stack<Integer>st=new Stack<>();
         for(int i=0;i<tokens.length;i++){
-            String ch=tokens[i];
+            String str=tokens[i];
 
-
-            if(ch.equals("+")){
+            if(str.equals("+")){
                 int a=st.pop();
                 int b=st.pop();
                 st.push(a+b);
             }
-            else if(ch.equals("-")){
+
+            else if(str.equals("-")){
                 int a=st.pop();
                 int b=st.pop();
                 st.push(b-a);
             }
-            else if(ch.equals("*")){
+            else if(str.equals("*")){
                 int a=st.pop();
                 int b=st.pop();
-                st.push(a*b);
+                st.push(b*a);
             }
-            else if(ch.equals("/")){
+            else if(str.equals("/")){
                 int a=st.pop();
                 int b=st.pop();
                 st.push(b/a);
             }
             else{
-                st.push(Integer.parseInt(tokens[i]));
+                st.push(Integer.parseInt(str));
             }
         }
         return st.peek();
