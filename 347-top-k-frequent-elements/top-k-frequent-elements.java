df0b165065721fe9ef08afe0ff3ec008cba392ae
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-03-12 09:23:45 +0530

    Time: 13 ms (76.21%) | Memory: 48.9 MB (43.27%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,27 +1,19 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        //since we have to find the frequency we can use a hasmap for this 
-
         HashMap<Integer,Integer>mp=new HashMap<>();
         for(int num:nums){
             mp.put(num,mp.getOrDefault(num,0)+1);
         }
         PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
-
         for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
             pq.add(new int[]{entry.getValue(),entry.getKey()});
         }
-
         while(pq.size()>k){
             pq.poll();
         }
-
         int[] res=new int[k];
-        int x=0;
-        while(!pq.isEmpty()){
-            res[x]=pq.poll()[1];
-            x++;
-
+        for(int i=0;i<k;i++){
+            res[i]=pq.poll()[1];
         }
         return res;
     }
