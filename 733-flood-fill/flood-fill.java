73c664a7094df2a88332f76271cf1697f5bbf01b
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-03-16 08:48:57 +0530

    Time: 1 ms (47.69%) | Memory: 45 MB (67.03%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,39 +1,31 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int[][] mat=new int[image.length][image[0].length];
+        int orgColor=image[sr][sc];
 
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[0].length;j++){
-                mat[i][j]=image[i][j];
-            }
-        }
-
-        int iniCol=image[sr][sc];
+        //can use the concept of bfs 
         int[] dx={-1,1,0,0};
         int[] dy={0,0,-1,1};
 
         Queue<int[]>q=new LinkedList<>();
         q.add(new int[]{sr,sc});
-        mat[sr][sc]=color;
+        image[sr][sc]=color;
         while(!q.isEmpty()){
-             int[] node=q.poll();
-             int r=node[0];
-             int c=node[1];
-
-             for(int i=0;i<4;i++){
-                int newR=r+dx[i];
-                int newC=c+dy[i];
-
-                if(newR<0|| newR>=image.length||newC<0 ||newC>=image[0].length||image[newR][newC]!=iniCol ||mat[newR][newC]==color){
+            int[] curr=q.poll();
+            int r=curr[0];
+            int c=curr[1];
+            for(int j=0;j<4;j++){
+                int newR=r+dx[j];
+                int newC=c+dy[j];
+                if(newR<0 ||newR>=image.length||newC<0 ||newC>=image[0].length||image[newR][newC]!=orgColor ||image[newR][newC]==color){
                     continue;
-
                 }
                 else{
-                    mat[newR][newC]=color;
+                    image[newR][newC]=color;
                     q.add(new int[]{newR,newC});
                 }
-             }
+
+            }
         }
-        return mat;
+        return image;
     }
 }
