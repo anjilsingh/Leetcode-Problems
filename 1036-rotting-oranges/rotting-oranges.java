c67a599a680b687372e57748de1cf767a693a839
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-03-16 08:36:30 +0530

    Time: 2 ms (69.96%) | Memory: 42.1 MB (93.28%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,48 +1,51 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        //first find the fresh orange count
         Queue<int[]>q=new LinkedList<>();
-        int row=grid.length-1;
-        int col=grid[0].length-1;
-        int fresh=0;
-        for(int i=0;i<=row;i++){
-            for(int j=0;j<=col;j++){
-              if(grid[i][j]==1) fresh++;
-
-              if(grid[i][j]==2){
-                q.add(new int[]{i,j});
-              }
+        int freshCount=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==2){
+                   q.add(new int[]{i,j});
+                }
+                else if(grid[i][j]==1){
+                    freshCount++;
+                }
             }
         }
-        int time=0;
+        if(freshCount==0) return 0;
         int[] dx={-1,1,0,0};
         int[] dy={0,0,-1,1};
 
-        while(fresh>0 && !q.isEmpty()){
+        //now we have to apply a bfs on queue size
+        int time=0;
+        while(!q.isEmpty()){
             int size=q.size();
-
             for(int i=0;i<size;i++){
-                int[] node=q.poll();
-                int r=node[0];
-                int c=node[1];
-
+                int curr[]=q.poll();
+                int r=curr[0];
+                int c=curr[1];
                 for(int j=0;j<4;j++){
-                   int  newR=r+dx[j];
-                   int  newC=c+dy[j];
-
-                    if(newR<0 ||newR>row||newC<0||newC>col||grid[newR][newC]==0||grid[newR][newC]==2){
+                    int newR=r+dx[j];
+                    int newC=c+dy[j];
+                    if(newR<0 ||newC<0 ||newR>=grid.length||newC>=grid[0].length||grid[newR][newC]==2||grid[newR][newC]==0){
                         continue;
                     }
                     else{
+                        q.add(new int[]{newR,newC});
                         grid[newR][newC]=2;
-                        q.add(new int[]{newR, newC});
-
-                        fresh--;
+                        freshCount--;
                     }
                 }
+
+
             }
             time++;
+            if(freshCount==0){
+                break;
+            }
         }
 
-        return fresh==0?time:-1;
+      return freshCount==0?time:-1;
     }
 }
