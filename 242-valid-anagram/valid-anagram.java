f7571d75dae93ceac6b95ab69094c5ccdd9f8556
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-03-17 05:38:15 +0530

    Time: 6 ms (43.15%) | Memory: 42.6 MB (98.92%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,24 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //if same characters 
-        //so can create two tables and do based on that 
-         
-         if(s.length()!=t.length()){
-            return false;
-         }
-
+        if(s.length()!=t.length()) return false;
         int[] arr=new int[26];
-   
-
-
         for(int i=0;i<s.length();i++){
             arr[s.charAt(i)-'a']++;
             arr[t.charAt(i)-'a']--;
         }
-
         for(int i=0;i<26;i++){
             if(arr[i]!=0){
-                return false;
+                  return false;
             }
         }
         return true;
