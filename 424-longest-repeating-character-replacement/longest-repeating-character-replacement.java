aa8b758bfe7c2417da3f9e0bf879d5d114e18991
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-03-17 07:11:06 +0530

    Time: 31 ms (25.89%) | Memory: 45.3 MB (18.57%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,21 +1,22 @@
 class Solution {
     public int characterReplacement(String s, int k) {
+        
         HashMap<Character,Integer>mp=new HashMap<>();
-        int maxFreq=0;
+        int len=0;
+        int mFreq=0;
         int l=0;
-        int length=Integer.MIN_VALUE;
         for(int r=0;r<s.length();r++){
             mp.put(s.charAt(r),mp.getOrDefault(s.charAt(r),0)+1);
-            maxFreq=Math.max(maxFreq,mp.get(s.charAt(r)));
+            mFreq=Math.max(mFreq,mp.get(s.charAt(r)));
 
-            if(r-l+1-maxFreq>k){
+            while(r-l+1-mFreq>k){
                 mp.put(s.charAt(l),mp.get(s.charAt(l))-1);
                 l++;
-                maxFreq=Math.max(maxFreq,mp.get(s.charAt(r)));
+                mFreq=Math.max(mFreq,mp.get(s.charAt(r)));
             }
-            length=Math.max(length,r-l+1);
-        }
+            len=Math.max(len,r-l+1);
 
-        return length;
+        }
+        return len;
     }
 }
