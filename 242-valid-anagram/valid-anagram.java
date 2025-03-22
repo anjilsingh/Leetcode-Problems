bace94f32654481c2e89f8674f3547d83b3f3d53
Anjil Singh <159554637+anjilsingh@users.noreply.github.com>
2025-03-22 18:52:45 +0530

    Time: 6 ms (43.12%) | Memory: 44.7 MB (44.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        //anagram means same character 
-        //so will do like that only 
-        
-        if(s.length()!=t.length()) return false;
-        int[] arr=new int[26];
-        for(int i=0;i<s.length();i++){
-            arr[s.charAt(i)-'a']++;
-            arr[t.charAt(i)-'a']--;
+
+        //tc o(log n)
+        if(s.length() !=t.length()){
+            return false;
         }
-        for(int i=0;i<26;i++){
-            if(arr[i]!=0){
+        char[] scharArr=s.toCharArray();
+        char[] tcharArr=t.toCharArray();
+        Arrays.sort(scharArr);
+        Arrays.sort(tcharArr);
+        String sortedS=new String(scharArr);
+        String sortedT=new String(tcharArr);
+        for(int i=0;i<s.length();i++){
+            if(sortedS.charAt(i)!=sortedT.charAt(i)){
                 return false;
             }
         }
